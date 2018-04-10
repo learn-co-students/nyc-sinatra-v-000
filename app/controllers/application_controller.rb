444a90b4d2c94a335e83197840b821c1e1ec62cc
Eden Mesfun <emesfun@gmail.com>
2018-04-10 20:24:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/new'
+  end
+
+  post '/figures/show' do
+
+    figure = Figure.new(name: params["figure"]["name"])
+    figure.save
+
+    # 1) FiguresController allows you to create a new figure with a title
+    #  Failure/Error: fill_in :figure_name, :with => "Doctor Who"
+    redirect "/figures/show" #moves to route GET '/figures/name'
+  end
+
+  get '/figures/show' do
+    erb :'figures/show'
+  end
 end
---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,4 @@
+class FiguresController < ApplicationController
+
+
+end
