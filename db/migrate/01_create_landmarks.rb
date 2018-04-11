534d00d677f92fe020bea4cf493d7a56aa5bf913
Erin Walker <erinb0317@hotmail.com>
2018-04-11 23:40:42 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,8 +2,9 @@ class CreateLandmarks < ActiveRecord::Migration[4.2]
   def change
     create_table :landmarks do |t|
       t.string :name
-      t.integer :figure_id
       t.integer :year_completed
+
+      t.timestamps
     end
   end
 end
