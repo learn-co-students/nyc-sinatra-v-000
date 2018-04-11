35caafbe6959acf33bd06f48bb8662ee40d6dbd8
Erin Walker <erinb0317@hotmail.com>
2018-04-11 02:11:02 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,12 @@
 class LandmarksController < ApplicationController
 
+  get '/' do
+    redirect '/landmarks'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/index'
+  end  
 end
