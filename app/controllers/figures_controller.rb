316f1930d31011042fc1df10e2f06b547d22cc47
Eden Mesfun <emesfun@gmail.com>
2018-04-11 16:24:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,32 @@
 class FiguresController < ApplicationController
 
 
+    get '/figures/new' do
+      @titles = Title.all
+      @landmarks = Landmark.all
+      @all_figures = Figure.all
+
+      erb :'figures/new'
+    end
+
+    post '/figures' do
+
+      landmark = Landmark.create(params["landmark"])
+
+      title = Title.find_by(id: params["figure"]["title_ids"])
+      figure = Figure.find_or_create_by(name: params["figure_name"])
+
+      figure.titles << title
+      figure.landmarks << landmark
+      figure.save
+
+      redirect "/figures/#{figure.id}" #moves to route GET '/figures/name'
+    end
+
+    get '/figures/:id' do
+
+      @figure = Figure.find_by_slug(params["slug"])
+      erb :'figures/show'
+    end
+
 end
