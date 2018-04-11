8ceb0d0913387838f7d391aedb6286ad025f819c
Eden Mesfun <emesfun@gmail.com>
2018-04-11 16:30:12 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,13 +11,17 @@ class FiguresController < ApplicationController
 
     post '/figures' do
 
-      landmark = Landmark.create(params["landmark"])
-
       title = Title.find_by(id: params["figure"]["title_ids"])
       figure = Figure.find_or_create_by(name: params["figure_name"])
 
       figure.titles << title
-      figure.landmarks << landmark
+
+      landmark_params = params["landmark"]
+
+      if !landmark_params.empty?
+          landmark = Landmark.create(params["landmark"])
+          figure.landmarks << landmark
+      end
       figure.save
 
       redirect "/figures/#{figure.id}" #moves to route GET '/figures/name'
