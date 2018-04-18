01a5c361b6bc1f75b28fa3390a2e83194e37d767
Araba Taylor <araba.taylor20@gmail.com>
2018-04-18 01:01:47 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,6 @@
 class FiguresController < ApplicationController
 
+
   get '/figures' do
     @figures = Figure.all
     erb :'/figures/index'
@@ -32,4 +33,22 @@ class FiguresController < ApplicationController
     redirect("/figures/#{@figure.id}")
   end
 
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params["figure"])
+    # @figure.title_ids = params["figure"]["title_ids"]
+    # @figure.landmark_ids = params["figure"]["landmark_ids"]
+    if params["title"]["name"] != ""
+      @figure.titles << Title.create(name: params["title"]["name"])
+    elsif params["landmark"]["name"] != ""
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
 end
