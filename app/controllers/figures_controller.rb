74904a1309f7bdc35841f35d5299055e5d583d9e
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-25 10:11:57 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,11 +16,10 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure=Figure.create(params[:figure])
-    binding.pry
-    @figure.titles=params[:figure][:title_ids]
-    @figure.landmarks=params[:figure][:landmark_ids]
+    #binding.pry
+    #@figure.titles=params[:figure][:title_ids]
+    #@figure.landmarks=params[:figure][:landmark_ids]
     #@figure.title=Title.find_or_create_by(params[:title])
-    binding.pry
     #@figure.landmark=Landmark.find_or_create_by(params[:landmark])
     redirect("/figure/#{@figure.slug}")
   end
