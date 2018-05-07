d393a8df10c26d32e7b9d30385a658150bc3fe50
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-05-07 02:03:10 +0000

    Done.

---
+++ db/migrate/20180506173726_create_figures_table.rb
@@ -0,0 +1,4 @@
+class CreateFiguresTable < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
---
+++ db/migrate/20180506173841_create_landmarks_table.rb
@@ -0,0 +1,4 @@
+class CreateLandmarksTable < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
