12e65405ecd82361e51529de1752e9b70be8e7ea
Elle Faraday <faraday.elle@gmail.com>
2018-05-09 22:49:14 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -47,7 +47,34 @@ class FiguresController < ApplicationController
   end
 
   post '/figures/:id' do
-
+    #get figure
+    @figure= Figure.find_by_id(params[:id])
+    #set name
+    @figure.name = params[:figure][:name]
+    #reset landmarks
+    @figure.landmarks.clear
+    #assign existing landmarks to figure
+    if params[:figure][:landmark_ids] != nil
+      params[:figure][:landmark_ids].each {|landmark| @figure.landmarks << Landmark.find_by_id(landmark)}
+    end
+    #create new landmark
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    #reset titles
+    @figure.titles.clear
+    #assign existing titles
+    if params[:figure][:title_ids] != nil
+      params[:figure][:title_ids].each {|title| @figure.titles << Title.find_by_id(title)}
+    end
+    #create new titles
+    if !params[:title][:name].empty?
+      @title = Title.create(name: params[:title][:name])
+      @figure.titles << @title
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
 end
