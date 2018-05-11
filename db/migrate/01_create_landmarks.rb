6be39398d1173546b83dcde4c5394e80e07f79fd
Ariana Berger <ariana.berger@gmail.com>
2018-05-11 15:57:24 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration[4.2]
-  raise "Write CreateLandmarks migration here"
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.string :figure_id
+      t.string :year_created
+    end
+  end
 end
