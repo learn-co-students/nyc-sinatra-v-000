1d03bea3fa966663a714134fe0cc8522c947642c
Ariana Berger <ariana.berger@gmail.com>
2018-05-12 15:58:33 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,21 @@
 class FiguresController < ApplicationController
 
-  get '/figures/new' do
+  get '/figures' do
+    @figures = Figures.all
+    erb :'/figures/index'
+  end
 
+  get '/figures/new' do
+    erb :'figures/new'
    end
+
+  post '/figures' do
+    @figure = Figure.create(:name => params[:figure][:name])
+    #create a new figure
+  end
+
+  get 'figures/:id' do
+    #find the post by id?
+  end
 
 end
