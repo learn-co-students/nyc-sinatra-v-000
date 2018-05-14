066f3b0fe05295ddec5f65058ff9322426058408
Dee Race <deerace12@gmail.com>
2018-05-14 15:47:31 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,17 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "landmarks/#{@landmark.id}"
+  end
+
 end
