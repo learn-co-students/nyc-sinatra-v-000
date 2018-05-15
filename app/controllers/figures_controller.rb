31554256281edc3e53d413eb1d0b9ac287b0a810
amanda johnson <manda.panda91@yahoo.com>
2018-05-15 21:55:43 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,15 @@
+class FiguresController < ApplicationController
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+    erb :'/figures/show'
+  end
+end
