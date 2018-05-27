8071bbaf7bc93fdf95e63bb681053acfd83ed112
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-27 00:07:54 +0000

    Done.

---
+++ db/migrate/04_create_figure_titles.rb
@@ -0,0 +1,14 @@
+class CreateFigureTitles < ActiveRecord::Migration
+
+  def up
+    create_table :figure_titles do |t|
+      t.integer :figure_id
+      t.integer :title_id
+    end
+  end
+
+  def down
+    drop_table :figure_titles
+  end
+
+end
