0456efbb441f9d850bd403da98c484fffbdeb3f5
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-28 01:31:06 +0000

    Done.

--- app/controllers/figuresController.rb
+++ app/controllers/figuresController.rb
@@ -1,21 +1,30 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
   post '/figures' do
-    puts "New Figure params=#{params}"
-    @figure = Figure.new(name: params[:figure_name])
-    @figure.landmark_ids = params[:landmarks]
-    if params[:new_landmark]
-      @figure.landmarks << Landmark.new(name: params[:new_landmark])
+    @figure = Figure.new(params[:figure])
+  #  @figure.landmark_ids = params[:landmarks]
+    if params[:landmark][:name]
+      @figure.landmarks << Landmark.new(params[:landmark])
     end
-    @figure.title_ids = params[:titles]
-    if params[:new_title]
-      @figure.titles << Title.new(name: params[:new_title])
+  #  @figure.title_ids = params[:titles]
+    if params[:title][:name]
+      @figure.titles << Title.new(params[:title])
     end
     @figure.save
     erb :'figures/show'
