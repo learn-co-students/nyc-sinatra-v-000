dfa3ac4a5777ad769a5efe2b879d5dae1e23e042
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-28 00:28:12 +0000

    Done.

--- app/controllers/figuresController.rb
+++ app/controllers/figuresController.rb
@@ -8,6 +8,16 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     puts "New Figure params=#{params}"
-    @figure = Figure.new(params[:figure_name])
+    @figure = Figure.new(name: params[:figure_name])
+    @figure.landmark_ids = params[:landmarks]
+    if params[:new_landmark]
+      @figure.landmarks << Landmark.new(name: params[:new_landmark])
+    end
+    @figure.title_ids = params[:titles]
+    if params[:new_title]
+      @figure.titles << Title.new(name: params[:new_title])
+    end
+    @figure.save
+    erb :'figures/show'
   end
 end
