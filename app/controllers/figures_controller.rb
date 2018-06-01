54a01fd5e0a679956945a99c88a2842822941e56
Sam. O <samrey2018@gmail.com>
2018-06-01 16:11:32 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,18 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+       @figures = Figure.all
+       # model name followed by a method
+       erb :'/figures/index'
+     end
+
   get '/figures/new' do
        @figures = Figure.all
        @titles  = Title.all
+       binding.pry
       erb :'/figures/new'
     end
+
 
  # :note type .... rspec spec/models ... TO pass the test by session
 end
