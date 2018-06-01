cd79a8682dc7d82e22b03a8bd9fc12ba7b01fcf7
Emily Jirles <emily.jirles@gmail.com>
2018-06-01 16:46:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
   configure do 
     use Rack::MethodOverride
+    set :public_folder, 'public'
     set :views, Proc.new { File.join(root, "../views/") }
     register Sinatra::Twitter::Bootstrap::Assets
   end 
