cafcf0c44752de2fa6d784cee154591782d3e9f8
Sam. O <samrey2018@gmail.com>
2018-06-02 23:12:40 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,34 +6,51 @@ class FiguresController < ApplicationController
        erb :'/figures/index'
      end
 
-  get '/figure/new' do
-       @figures = Figure.all
+  get '/figures/new' do
        @titles  = Title.all
        @landmarks = Landmark.all
-      erb :'/figure/new'
+      erb :'/figures/new'
     end
 
  # :note type .... rspec spec/models ... TO pass the test by session
 
 
- post '/figure' do
+ post '/figures' do
 #create a new artist
 #need a flash message...
-
      @figure = Figure.create(name: params[:figure_name])   # This is where we set the name for song/ it want us to pass in an hash.
      @figure.titles << Title.find_or_create_by(params[:title])  # shovel in Title into figure.titles to be used in the views folder
      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
      @figure.save
 
-   redirect to ("/figure/#{@figure.id}")  # "/figure/[name of newly created figure]
+   redirect to ("/figures/#{@figure.id}")  # "/figure/[name of newly created figure]
 end
 
 
- get '/figure/:id' do
+ get '/figures/:id' do
       @figure = Figure.find_by_id(params[:id])  # slug helps to find by name instaed of ID
       # model name followed by a method
-      erb :'/figure/show'
+      erb :'/figures/show'
  end
 
+   get '/figures/:id/edit' do
+       @figure = Figure.find_by_id(params[:id])
+      #  slug helps to find by name instaed of ID
+       erb :'figures/edit'
+    end
+
+ patch '/figures/:id' do
+        @figure = Figure.find_by_id(params[:id])
+        @figure.update(params[:figure])
+        @figure.titles = Titles.find_or_create_by(name: params[:titles][:name])
+        @figure.landmarks.ids = params[:landmark]
+        @figure.save
+
+        #  flash[:message] = "Successfully updated song."
+        # redirect to("/songs/#{@song.slug}")
+
+          # end
+
+      end
 
 end
