fb607e8381daf82deee980d8dc743fff1b38b980
Sam. O <samrey2018@gmail.com>
2018-06-06 00:14:28 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,22 +6,40 @@ class LandmarksController < ApplicationController
    end
 
    get  '/landmarks/new' do
-      @landmarks = Landmark.all
+           Landmark.all
       erb :'/landmarks/new'
     end
 
+   post '/landmarks' do
+     Landmark.create(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
+   #  @landmark.save
+   redirect to '/landmarks'
+   end
+
     get '/landmarks/:id' do
-        @landmark = Landmark.find_by_id(params[:id])
+        @landmark = Landmark.find(params[:id])
         # model name followed by a method
         erb :'/landmarks/show'
      end
-
 
      get '/landmarks/:id/edit' do
         @landmark = Landmark.find(params[:id])
       #  slug helps to find by name instaed of ID
         erb :'/landmarks/edit'
     end
+
+   patch "/landmarks/:id" do
+          @landmark = Landmark.find(params[:id])
+   # We be spcific about what is changed rather than
+#   just "update"
+
+          @landmark.name = params[:landmark][:name]
+          @landmark.year_completed = params[:landmark][:year_completed]
+
+          @landmark.save
+
+         redirect to "/landmarks/#{@landmark.id}"
+     end
 
 
 end
