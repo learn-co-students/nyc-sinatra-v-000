246ee4d429c1d4719caa38c705a9733a625250d7
Victoria Meng <nights1ststar@protonmail.com>
2018-06-07 00:56:25 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,4 +2,15 @@ class LandmarksController < ApplicationController
   get "/landmarks" do
     erb :"/landmarks/index"
   end
+
+  get "/landmarks/new" do
+    erb :"/landmarks/new"
+  end
+
+  post "/landmarks/new" do
+    @landmark = Landmark.find_or_create_by(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+
 end
