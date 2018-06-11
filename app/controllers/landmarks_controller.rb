29e5f71e1a6d0bab1849b3bb0934261c58e4e23a
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-11 16:54:11 -0400

    ATP

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,6 +3,27 @@ class LandmarksController < ApplicationController
         erb :'/landmarks/new'
     end
 
+    get '/landmarks/:id' do
+        @landmark = Landmark.find(params[:id])
+
+        erb :'/landmarks/show'
+    end
+
+    patch '/landmarks/:id' do
+        landmark = Landmark.find(params[:id])
+        landmark.name = params[:landmark][:name]
+        landmark.year_completed = params[:landmark][:year_completed].to_i
+        landmark.save
+
+        redirect to "/landmarks/#{landmark.id}"
+    end
+
+    get '/landmarks/:id/edit' do
+        @landmark = Landmark.find(params[:id])
+
+        erb :'/landmarks/edit'
+    end
+
     post '/landmarks' do
         landmark = Landmark.new
         landmark.name = params[:landmark][:name]
@@ -14,7 +35,7 @@ class LandmarksController < ApplicationController
 
     get '/landmarks' do
         @landmarks = Landmark.all
-        
+
         erb :'/landmarks/index'
     end
 end
