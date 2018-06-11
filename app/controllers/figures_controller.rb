8952e680b4d3e549b238ef04a50b27ca4ad335bc
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-11 18:18:51 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,4 +4,27 @@ class FiguresController < ApplicationController
     @titles = Title.all
     erb :'/figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if params[:title][:name]!=nil
+      title = Title.create(params[:title])
+      @figure.titles << title
+    end
+    if params[:landmark][:name]!=nil
+      new_l = Landmark.create(params[:landmark])
+      @figure.landmarks << new_l
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'/figures/show'
+  end
 end
