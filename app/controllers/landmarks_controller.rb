e799aa69f65f86f1019d3f8d7d217b1e5d86b332
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-11 16:40:30 -0400

    Implemneted form for creating new landmarks

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
 class LandmarksController < ApplicationController
+    get '/landmarks/new' do
+        erb :'/landmarks/new'
+    end
 
+    post '/landmarks' do
+        landmark = Landmark.new
+        landmark.name = params[:landmark][:name]
+        landmark.year_completed = params[:landmark][:year_completed].to_i
+        landmark.save
+
+        redirect to "/landmarks/#{landmark.id}"
+    end
 end
