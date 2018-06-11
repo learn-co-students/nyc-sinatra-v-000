ff7d5812075b967496a940e36943444b7492a4dc
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-11 20:35:31 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
 
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
 end
