7ebdb7d70db8c84c0e79c413a96d4e50fd6cbe08
Bryan Hooper <bryan@lindsayfarm.net>
2018-06-15 10:11:38 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,43 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    new_landmark = Landmark.new(name: params['landmark']['name'], year_completed: params['landmark']['year_copmleted'])
+
+    new_landmark.save
+
+    redirect to "landmark/#{new_figure.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+
+
+  patch '/landmarks/:id' do 
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+
+
+
 
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change 
+    create_table :landmarks do |t|
+      t.string :name
+      t.belongs_to :figure
+      t.string :year_copmleted
+    end
+  end
+
 end
