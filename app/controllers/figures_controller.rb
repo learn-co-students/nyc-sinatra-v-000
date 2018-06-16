ae390987614ff9fc18819318fada38b59c34658a
David Markel <davidgmarkel@gmail.com>
2018-06-16 20:02:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,24 +11,17 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     #creates a new Figure instance if one for that name doesn't already exist
-    @figure = Figure.find_or_create_by(name: params[:figure][:name])
+    @figure = Figure.create(params["figure"])
 
-    #if any title checkboxes are selected, each is associated to @figure
-    if params[:figure][:title_ids]
-      params[:figure][:title_ids].each {|title| @figure.titles << Title.find(title)}
     #if a unique title is entered in the title textfield, a new Title instance is created/associated to @figure
-    elsif !params[:title][:name].empty?
-      @figure.titles << Title.find_or_create_by(name: params[:title][:name])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(params[:title])
     end
-
-    #if any landmark checkboxes are selected, each is associated to @figure
-    if params[:figure][:landmark_ids]
-      params[:figure][:landmark_ids].each {|landmark| @figure.landmarks << Landmark.find(landmark)}
     #if a unique landmark is entered in the landmark textfield, a new Landmark instance is created/associated to @figure
-    elsif !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
     end
-
+    @figure.save
     redirect to :"/figures/#{@figure.id}"
   end
 
@@ -44,33 +37,11 @@ class FiguresController < ApplicationController
 
   post '/figures/:id' do
     @figure = Figure.find(params[:id])
-    @figure.update(name: params[:figure][:name])
-
-    #if any checkboxes are selected, figure.titles is cleared and replaced with new selections
-    if params[:figure][:title_ids]
-      @figure.titles.clear
-      params[:figure][:title_ids].each do |title|
-          @figure.titles << Title.find(title)
-      end
-    #if no checkboxes are selected, figure.titles is cleared
-    elsif !params[:figure][:title_ids]
-      @figure.titles.clear
-    end
+    @figure.update(params[:figure])
 
     #if a unique title is entered into the title textfield, it is associated to @figure
     if !params[:title][:name].empty?
       @figure.titles << Title.find_or_create_by(name: params[:title][:name])
-    end
-
-    #if any checkboxes are selected, figure.landmarks is cleared and replaced with new selections
-    if params[:figure][:landmark_ids]
-      @figure.landmarks.clear
-      params[:figure][:landmark_ids].each do |landmark|
-        @figure.landmarks << Landmark.find(landmark)
-      end
-    #if no checkboxes are selected, figure.landmarks is cleared
-    elsif !params[:figure][:landmark_ids]
-      @figure.landmarks.clear
     end
 
     #if a unique landmark is entered into the landmark textfield, a new landmark is created/associated to @figure
