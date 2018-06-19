8c415c1210c9e26ddcd10cc115def711368973f5
arye-dov-eidelman <aryedov.e@gmail.com>
2018-06-18 23:38:03 -0400

    Done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,50 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.new(params[:figure])
+    if !params[:title][:name].strip.empty?
+      figure.titles << Title.find_or_create_by(params[:title])
+    end
+    if !params[:landmark][:name].strip.empty?
+      figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+    figure.save
+    redirect "/figures/#{figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id]) || (return 404)
+    erb :'figures/show'
+  end
+  
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id]) || (return 404)
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find_by(id: params[:id]) || (return 404)
+    figure.update(params[:figure])
+    if !params[:title][:name].strip.empty?
+      figure.titles << Title.find_or_create_by(params[:title])
+    end
+    if !params[:landmark][:name].strip.empty?
+      figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+    figure.save
+    redirect "/figures/#{params[:id]}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,44 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.new(params[:landmark])
+    if params[:figure] && !params[:figure][:name].strip.empty?
+      landmark.figure == Title.find_or_create_by(params[:figure])
+    end
+    landmark.save
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id]) || (return 404)
+    erb :'landmarks/show'
+  end
+  
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params[:id]) || (return 404)
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find_by(id: params[:id]) || (return 404)
+    landmark.update(params[:landmark])    
+    if params[:figure] && !params[:figure][:name].strip.empty?
+      landmark.figure == Figure.create(params[:figure])
+    end
+    landmark.save
+    redirect "/landmarks/#{params[:id]}"
+  end
 end
