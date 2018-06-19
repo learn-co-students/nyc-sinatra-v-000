1fb2eb97607dda4b40945fec76093d4dca9fddba
Michael Kang <michaelkang85@gmail.com>
2018-06-19 02:42:59 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,15 @@
 class FiguresController < ApplicationController
   get '/figures/new' do
     @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
   end
 
   post '/figures' do
-    erb :'figures/show'
+    @figure = Figure.create(name: params[:figure][:name])
+    @figure.titles << Title.find_or_create_by(id: params[:figure][:title_ids])
+
+    #@figure.landmarks << Landmark.find_or_create_by()
+    erb :'/figures/show'
   end
 end
