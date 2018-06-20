2c1c742f0346a737e31fe5ace7e1af66a61f50ee
Michael Kang <michaelkang85@gmail.com>
2018-06-20 01:09:26 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,17 +7,31 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(name: params[:figure][:name])
-    if !params[:title][:name].empty?
-    @figure.titles << Title.create(name: params[:title][:name])
-    end
-    if !params[:figure][:title_ids].empty?
-      params[:figure][:title_ids].each do |title_id|
-      @figure.titles << Title.find(title_id)
+
+#<--------------TITLES--------------->
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(name: params[:title][:name])
       end
-    end
-    binding.pry
-    @figure.landmarks << Landmark.find_or_create_by(@figure.id)
-    #@figure.landmarks << Landmark.find_or_create_by()
-    erb :'/figures/show'
+
+      if !params[:figure][:title_ids].empty?
+        params[:figure][:title_ids].each do |title_id|
+          @figure.titles << Title.find(title_id)
+        end
+      end
+#<---------------------LANDMARKS------------------->
+binding.pry
+      if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+      end
+
+      if !params[:figure][:landmark_ids].empty?
+        params[:figure][:landmark_ids].each do |landmark_id|
+          @figure.landmarks << Landmark.find(landmark_id)
+        end
+      end
+      erb :'/figures/show'
   end
+
+
+
 end
