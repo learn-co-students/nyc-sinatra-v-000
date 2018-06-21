051b3b438cc469ca42d49f7a1054de2a73511a9b
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 02:58:44 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,12 +2,22 @@ class FiguresController < ApplicationController
 
   get '/figures' do
     @figures = Figure.all
-    erb :index
+    erb :'/figures/index'
   end
 
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
-    erb :new
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.create(name: params["figure"]["name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params["genres"]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 end
