2f38482c4fe5254f01a505ed58454cda0ec1a766
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 04:45:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,7 +12,8 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(name: params["figure"]["name"])
+    binding.pry
+    @figure = Figure.create(params["figure"])
     if !params["title"]["name"].empty?
       @figure.titles << Title.create(name: params["figure"]["title_ids"])
     end
@@ -25,12 +26,14 @@ class FiguresController < ApplicationController
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.landmarks
-    @figure.
+    @figure.titles
     erb :'/figures/show'
   end
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/edit'
   end
 
