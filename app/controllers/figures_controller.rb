66d86b49ef63e66023c16b64d5fa5d516bc2d4be
Eddie Moon <eddiemoonie@gmail.com>
2018-06-27 19:37:38 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,10 +16,16 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'figures/edit'
+  end
+
   post '/figures' do
     @figure = Figure.create(params["figure"])
     if !params[:title][:name].empty?
-      @figure.titles << Title.create(params[:title])
+      @figure.titles << Title.create(params["new_title"])
     end
 
     if !params[:landmark][:name].empty?
@@ -31,9 +37,20 @@ class FiguresController < ApplicationController
     redirect("/figures/#{@figure.id}")
   end
 
-  get '/figures/:id/edit' do 
+  patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
 
-    erb :'figures/edit'
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+
+    redirect("/figures/#{@original_figure.id}")
   end
 end
