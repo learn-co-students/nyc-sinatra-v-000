76635ec15af1b262d1eac1f0bfd956de0d6f741d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-27 18:14:27 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,8 +1,18 @@
 class LandmarksController < ApplicationController
 
   get '/landmarks/new' do
-    
     erb :'landmarks/new'
   end
+  
+  post '/landmarks' do 
+    @landmark = Landmark.create(params[:landmark])
+  end
+  
+  get '/landmarks' do 
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+  
+ 
   
 end
