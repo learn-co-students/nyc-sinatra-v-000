9dac193d27b00359935af77b9eec92cdb4dbc902
Karl Amoguis <ktamoguis@gmail.com>
2018-06-27 04:03:26 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,12 +11,32 @@ class FiguresController < ApplicationController
     #binding.pry
     @titles = Title.all
     @landmarks = Landmark.all
-    binding.pry
     @figure = Figure.create(params[:figure])
+    if params[:title][:name] == ""
+    else
+      @newtitle = Title.create(name: params[:title][:name])
+      @newtitle.save
+      @figure.titles << @newtitle
+    end
+
+    if params[:landmark][:name] == ""
+    else
+      @newlandmark = Landmark.create(name: params[:landmark][:name])
+      @newlandmark.save
+      @figure.landmarks << @newlandmark
+    end
 
     @figure.save
+  end
 
-    #binding.pry
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'figures/index'
+  end
+
+  get '/figures/:figure_id' do
+
   end
 
 end
