4f15f35a4ddfb54460faf287b9140097f8fe5599
Isaac Villicana <villicana.i@gmail.com>
2018-06-29 00:48:48 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,46 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    lm = Landmark.create(params[:landmark])
+    if !params[:figure][:name].empty?
+      lm.figure = Figure.create(params[:figure])
+    end
+    #if !params[:landmark][:name].empty?  && !params[:landmark][:year_completed].empty?
+    #  figure.landmarks << Landmark.create(params[:landmark])
+    #end
+    lm.save
+
+    redirect "landmarks/#{lm.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    
+    lm = Landmark.find(params[:id])
+    lm.update(params[:landmark])
+    if !params[:figure][:name].empty?
+      lm.figure = Figure.create(params[:figure])
+    end
+    lm.save
+
+    redirect "landmarks/#{lm.id}"
+  end
+
 end
