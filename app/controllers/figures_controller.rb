c0238285d4c38eec08639d46f295c2f78ac82e47
taryn yaeger <tsyaeger@gmail.com>
2018-07-05 23:01:22 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,14 @@
 class FiguresController < ApplicationController
-
+  
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+  
 end
+
