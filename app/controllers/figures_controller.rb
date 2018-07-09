62d0151a3fcb14d11ba7b7124fab8df708103a18
taryn yaeger <tsyaeger@gmail.com>
2018-07-09 01:09:23 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,15 +5,25 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
   
+  
+  
+  {"figure"=>
+    {
+      "name"=>"name", "title_ids"=>["3"], 
+      "landmark_ids"=>["1"]
+    }, 
+      "new_title"=>{"name"=>"person"}, 
+      "new_landmark"=>{"name"=>"place"}, 
+      "_new_landmark"=>{"year"=>"560"}
+    }
+  
+  
   post '/figures' do 
     raise params.inspect
     fig_name = params[:figure][:name]
     land_name = params[:landmark][:name]
     land_year = params[:landmark][:year]
     title_name = params[:title][:name]
-    
-
-    
 
     @figure = Figure.create(:name => fig_name)
     @landmark = Landmark.create(:name => land_name, :year_completed => land_year)
@@ -21,6 +31,15 @@ class FiguresController < ApplicationController
     "#{@figure.name} #{@landmark.name} #{@landmark.year_completed} #{@title.name}"
     # redirect to "figures/#{@figure.id}"
   end
+  
+  
+  {"figure"=>{"name"=>"ty", "title_ids"=>["1"], "landmark_ids"=>["3"]}, "title"=>{"name"=>"may"}, "landmark"=>{"name"=>"flat", "year"=>"1200"}}
+  
+  
+  
+  
+  
+  
   
   get '/figures/new' do 
     @figures = Figure.all
