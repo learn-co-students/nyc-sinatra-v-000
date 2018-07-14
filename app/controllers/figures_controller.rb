08ce84504fd88c0fca88aa5d26c2109d9c28d094
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-14 22:03:36 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,4 +2,10 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.create(params[:id])
+    @figure.save
+    erb :'figures/show'
+  end
 end
