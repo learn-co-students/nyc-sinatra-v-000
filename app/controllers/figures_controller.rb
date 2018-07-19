8ec9bad035a805a020c010597f323102db25bd68
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-19 18:40:16 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,13 +5,20 @@ class FiguresController < ApplicationController
   end
 
   get "/figures/:id" do
-  #find id and define class variable for object
-    erb :"figures/:id"
+    @figure =Figure.find_by(id: params[:id])
+    erb :"figures/single"
   end
 
   post "/figures" do
-    
-    redirect "/figures/:id"
+    new_figure = Figure.create(params[:figure])
+    new_figure.titles << Title.find_or_create_by(params[:title])
+    new_figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    redirect "/figures"
+  end
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :"/figures/show"
   end
 
 
