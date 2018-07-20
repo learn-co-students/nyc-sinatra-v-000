285ee49e1aab7e790d9d6d6dad28359401cbce68
Maverick Tesini <mavericktesini@gmail.com>
2018-07-20 06:27:39 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,18 +7,26 @@ class FiguresController < ApplicationController
 
   get '/figures/new' do #create page request
     @titles = Title.all
-    @landmarks = Landmark.all
     erb :"figures/new"
   end
 
   post '/figures' do #create and redirect
-    @figure = Figure.create(name: params[:figure][:name])
+    @figure = Figure.create(params[:figure])
+
+    if !params[:title][:name].empty? #hence user fills out new entry
+      #creation handles id automatically
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty? #NOT empty
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+
     redirect "/figures/#{@figure.slug}"
   end
 
   get '/figures/:slug' do #Show page request
     @figure = Figure.find_by_slug(params[:slug])
-    @title = Title.find_by_id(params[:title])
 
     erb :"figures/show"
   end
