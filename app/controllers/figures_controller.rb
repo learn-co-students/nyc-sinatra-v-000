1c0d0b011caa19260f26e16b203d58c97459adcf
Carrie Savinell <carrielane89@gmail.com>
2018-07-20 12:41:51 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
 
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    
+  end
 end
