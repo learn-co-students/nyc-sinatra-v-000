a49323484f1c28808fe401ad400b21b9aafd0e18
Kayla Reopelle <kreopelle@gmail.com>
2018-07-21 04:55:49 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,17 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    erb :'figures/new', :layout => :layout
+  end
+
   get '/figures' do
+    @figures = Figure.all
     erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
   end
 
 end
