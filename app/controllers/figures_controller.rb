f165a5621fe6d6d1bf890b0c96c93167289aa8c7
github username <github email address>
2018-07-22 23:10:24 +0200

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,16 +1,16 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-    binding.pry
     erb :index
   end
 
   get '/figures/new' do
+
     erb :'figures/new'
   end
 
   post '/figures' do
-
+     binding.pry
   end
 
 end
