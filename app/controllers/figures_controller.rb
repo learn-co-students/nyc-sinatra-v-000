6f9c96743d149349b82e279affe96452a7f0c8c2
Elaine Anderson <eparie@bu.edu>
2018-07-23 13:47:51 +0000

    saftey

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,18 +1,27 @@
 class FiguresController < ApplicationController
 
-  get '/figures' do
-    erb :'figures/index'
-  end
-
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'figures/new'
   end
 
+  get '/figures' do
+    @figures = Figure.all
+   erb :'figures/index'
+   end
+
+
   post '/figures' do
+      @figure = Figure.create(:name => params["figure"]["name"])
+      if !params["title"]["name"].empty?
+      @title = Title.create(:name => params["title"]["name"])
+      @figure.title_id << @title.id
+    else
+     @figure.titles << Title.find_by(:name => params["title"]["name"])
+    end
+  @figure.save
     erb :'figures/show'
   end
-
 
 end
