63215549927ebdfdef1906b1a186655766222847
Pamela Frieze <pfrieze@gmail.com>
2018-07-25 01:41:54 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do
+      @landmark = Landmark.create(params["landmark"])
+      if !params[:figure][:name].empty?
+        @landmark.figure = Figure.create(params[:figure][:name])
+      end
+
+      @landmark.save
+      redirect to "/landmarks/#{@landmark.id}"
+    end
 end
