acb23dbbcaa65e170c5b2409f797b5bb5ced16dc
Kachel Wittig <kjwittig@gmail.com>
2018-07-31 14:43:14 -0500

    completed figures controller

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @figure = @landmark.figures.first.name
+
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    #figure?
+    @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
