b7defb92db23b69f8e8fc4da6cdd788c1f7a7274
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-02 00:24:28 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+       @figures = Figure.all
+       erb :'figures/index'
+     end
+
+     get '/figures/new' do
+       erb :'figures/new'
+     end
+
+     post '/figures' do
+       @figure = Figure.create(name: params["figure"]["name"])
+       @figure.landmark = Figure.create(landmark: params["landmark"]["name"])
+       @figure.title = Figure.create(title: params["title"]["name"])
+       @figure.save
+       end
 end
