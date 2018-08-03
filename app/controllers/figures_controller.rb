73ce0bcad0bedfa919a942ce711162dfd3c72e2c
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-03 18:12:55 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -19,4 +19,18 @@ class FiguresController < ApplicationController
        erb :'figures/edit'
      end
 
+     post '/figures' do
+       @figure = Figure.create(params["figure"])
+       if !params[:landmark][:name].empty?
+         @figure.landmarks << Landmark.create(params[:landmark])
+       end
+
+       if !params[:title][:name].empty?
+         @figure.titles << Title.create(params[:title])
+       end
+
+       @figure.save
+       redirect to "/figures/#{@figure.id}"
+     end
+
  end
--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,7 +1,7 @@
 class Figure <ActiveRecord::Base
 
   has_many :landmarks
-  has_many :figure_titles
+  has_many :figure_title
   has_many :titles, :through => :figure_title
 
 end
