b78fda28f20cea1be05816b54c4d0668a79dfdb8
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-07 19:48:55 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,7 +10,7 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/:id' do
-    @current_figure = Figure.find_by_id(params[:id])
+    @figure = Figure.find_by_id(params[:id])
     erb :"/application/figures/show"
   end
 
@@ -24,7 +24,7 @@ class FiguresController < ApplicationController
       end
     end
 
-    if params[:title][:name]
+    if params[:title][:name] != ""
       @title = Title.create(name: params[:title][:name])
       @figure.titles << @title
     end
@@ -36,14 +36,52 @@ class FiguresController < ApplicationController
       end
     end
 
-    if params[:landmark][:name]
+    if params[:landmark][:name] != ""
       @landmark = Landmark.create(name: params[:landmark][:name])
       @figure.landmarks << @landmark
     end
     @figure.save
+
+    redirect "/figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
     erb :'/application/figures/edit'
+  end
+
+  patch '/figures/:id/edit' do
+    @original_figure = Figure.find_by_id(params[:id])
+    @original_figure.name = params[:figure][:name]
+    @original_figure.titles.clear
+    @original_figure.landmarks.clear
+
+    if params[:figure][:title_ids]
+      params[:figure][:title_ids].each do |id|
+        @title = Title.find_by_id(id)
+        @original_figure.titles << @title
+      end
+    end
+
+
+    if params[:title][:name] != ""
+      @title = Title.create(name: params[:title][:name])
+      @original_figure.titles << @title
+    end
+
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each do |id|
+        @landmark = Landmark.find_by_id(id)
+        @original_figure.landmarks << @landmark
+      end
+    end
+
+    if params[:landmark][:name] != ""
+      @landmark = Landmark.create(name: params[:landmark][:name])
+      @original_figure.landmarks << @landmark
+    end
+
+    @original_figure.save
+    redirect "/figures/#{@original_figure.id}"
   end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,7 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :"/application/landmarks/new"
+  end
+
 end
