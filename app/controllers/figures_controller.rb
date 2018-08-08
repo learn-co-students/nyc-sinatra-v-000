d111e9b36ff9e12c35fbddac948b99a0d9dc5f57
Kevin Chi <kevinchi118@gmail.com>
2018-08-08 01:12:03 +0000

    figures index and show

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,14 @@
 class FiguresController < ApplicationController
-end 
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  
+end
--- config.ru
+++ config.ru
@@ -8,6 +8,5 @@ if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
 end
 
 use LandmarksController
-use TitlesController
 use FiguresController
 run ApplicationController
