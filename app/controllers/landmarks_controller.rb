37886f08760d6ee7d5ae779376d408d9e2268293
Kevin Chi <kevinchi118@gmail.com>
2018-08-08 01:47:49 +0000

    landmarks index and show

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,9 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'landmarks/index'
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end 
 end
