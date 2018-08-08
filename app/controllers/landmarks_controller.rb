0e6f51d7c9a7760f4faadf507cdc094dfbcb15e5
Kevin Chi <kevinchi118@gmail.com>
2018-08-08 02:54:42 +0000

    landmarks new and edit

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,8 +5,32 @@ class LandmarksController < ApplicationController
     erb :'landmarks/index'
   end
 
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
   get '/landmarks/:id' do
     @landmark = Landmark.find_by_id(params[:id])
     erb :'landmarks/show'
-  end 
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks' do
+    Landmark.create(params[:landmark])
+    redirect '/landmarks'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+
 end
