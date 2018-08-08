ca6d21a7f665abaf13e0b41f3e73e4b883efcdb9
Kevin Chi <kevinchi118@gmail.com>
2018-08-08 16:46:21 +0000

    Figures new

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,10 +5,29 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id])
-    erb :'/figures/show'
+  get '/figures/new' do
+    erb :'figures/new'
   end
 
-  
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:title].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+
+    if !params[:landmark].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
