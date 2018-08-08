d882b377b00c9f996028ad7d9f21d8166d2fdace
Kevin Chi <kevinchi118@gmail.com>
2018-08-08 18:51:31 +0000

    figures edit

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,6 +14,11 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
+
   post '/figures' do
     @figure = Figure.create(params[:figure])
 
@@ -28,6 +33,35 @@ class FiguresController < ApplicationController
     end
 
     redirect "/figures/#{@figure.id}"
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    # @figure.titles.clear
+    # params[:figure][:title_ids].each do |title_id|
+    #   @figure.titles << Title.find_by_id(title_id)
+    # end
+    #
+    # @figure.landmarks.clear
+    # params[:figure][:landmark_ids].each do |landmark_id|
+    #   @figures.landmarks << Landmark.find_by_id(landmark_id)
+    # end
+
+    if !params[:title].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+
+    if !params[:landmark].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+
+    @figure.save
+    erb :'/figures/show'
   end
 
 end
