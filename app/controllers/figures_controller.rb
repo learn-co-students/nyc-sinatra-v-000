026467fcb66314813ee6ac1d59ecf45533856d54
Anu Khambete <anukhambete@gmail.com>
2018-08-09 23:01:25 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -20,9 +20,34 @@ class FiguresController < ApplicationController
   post '/figures' do
     #binding.pry
     @figure = Figure.create(name: params[:figure_name])
-    @figure.title_ids = params[:figure][:title_ids]
-    @figure.landmark_ids = params[:figure][:landmark_ids]
+    #@figure.title_ids = params[:figure][:title_ids]
+    #@figure.landmark_ids = params[:figure][:landmark_ids]
+
+    if params.keys.include?("figure") && !params[:figure][:title_ids].blank?
+      @figure.title_ids = params[:figure][:title_ids]
+      @figure.save
+    elsif params.keys.include?("title") && !params[:title][:name].blank?
+      @title = Title.find_or_create_by(name: params[:title][:name])
+      @figure.titles << @title
+      @title.save
+    else
+      @figure.save
+    end
+
+    if params.keys.include?("figure") && !params[:figure][:landmark_ids].blank?
+      @figure.landmark_ids = params[:figure][:landmark_ids]
+      @figure.save
+    elsif params.keys.include?("landmark") && !params[:landmark][:name].blank?
+      @landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      @figure.landmarks << @landmark
+      @landmark.save
+    else
+      @figure.save
+    end
+
+
     @figure.save
+
   end
 
 end
