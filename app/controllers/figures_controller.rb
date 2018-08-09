6d36a51ea8cc8a30b5f6450eb7fe6f5bc2ddd757
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-09 23:30:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,15 @@
 class FiguresController < ApplicationController
 
   get '/figures/new' do
+    
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
   end
+
+  post '/figures' do
+    binding.pry
+  end
+
 end
