6c1769ada1ebe4b26e69210e52f226672420f447
Anu Khambete <anukhambete@gmail.com>
2018-08-09 21:46:28 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,10 +4,25 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
-  get '/figures/:slug' do
-    @figure = Figure.find_by_slug(params[:slug])
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    #binding.pry
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
     #binding.pry
     erb :'/figures/show'
+  end
+
+  post '/figures' do
+    #binding.pry
+    @figure = Figure.create(name: params[:figure_name])
+    @figure.title_ids = params[:figure][:title_ids]
+    @figure.landmark_ids = params[:figure][:landmark_ids]
+    @figure.save
   end
 
 end
