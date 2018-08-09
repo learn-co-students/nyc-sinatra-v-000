bfa490adb7c277c7f627a448652375b83e77e40a
Anu Khambete <anukhambete@gmail.com>
2018-08-09 20:04:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,13 @@
 class FiguresController < ApplicationController
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:slug' do
+    @figure = Figure.find_by_slug(params[:slug])
+    #binding.pry
+    erb :'/figures/show'
+  end
 
 end
--- app/models/figure.rb
+++ app/models/figure.rb
@@ -2,4 +2,30 @@ class Figure <ActiveRecord::Base
 has_many :landmarks
 has_many :figure_titles
 has_many :titles, through: :figure_titles
+
+  def slug
+    a = self.name.downcase
+    #binding.pry
+    if a.gsub!(/[!@% &"]/,'-')
+      slug = a
+      #binding.pry
+    else
+      slug = a
+    end
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    value = nil
+    Figure.all.each do |figure|
+      #binding.pry
+      if figure.slug == slug
+        #binding.pry
+        #val = artist
+        value = figure
+      end
+    end
+    value
+  end
+
 end
