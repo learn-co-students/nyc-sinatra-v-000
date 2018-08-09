fb0855359f21fe1b5abfb8ceda6d4f08673373cf
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-09 23:34:01 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,11 @@
 class FiguresController < ApplicationController
 
   get '/figures/new' do
-    
+
     @figures = Figure.all
     @titles = Title.all
     @landmarks = Landmark.all
+    binding.pry
     erb :'/figures/new'
   end
 
