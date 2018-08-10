3ea7319e2a1c0ba6f176819bb9e6d4bb3cb611b6
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-10 18:20:37 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks/new' do
+    
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
 end
