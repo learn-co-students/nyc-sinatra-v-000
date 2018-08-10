d9e1e774908cf32c32dc66365aa70096e55af673
Anu Khambete <anukhambete@gmail.com>
2018-08-10 01:01:40 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,24 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    #binding.pry
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    #binding.pry
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    
+  end
+
 
 end
