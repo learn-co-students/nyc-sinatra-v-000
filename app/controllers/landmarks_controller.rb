bc69efee5450cb1f1d3f82a8b46e10baee0dfd47
Anu Khambete <anukhambete@gmail.com>
2018-08-10 01:27:57 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -16,6 +16,12 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/show'
   end
 
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    binding.pry
+    erb :'/landmarks/edit'
+  end
+
   post '/landmarks' do
     #binding.pry
     @landmark = Landmark.find_or_create_by(name: params[:landmark_name], year_completed: params[:landmark_year_completed])
