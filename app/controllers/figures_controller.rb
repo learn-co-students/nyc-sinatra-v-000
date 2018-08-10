4a5f008285a27b18f86708e7bd45e4b4f5a15f26
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-10 11:00:21 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,21 +1,29 @@
 class FiguresController < ApplicationController
 
-
   get '/figures' do
     @figures = Figure.all
     erb :'/figures/index'
   end
 
   get '/figures/new' do
+
     @figures = Figure.all
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'/figures/new'
   end
 
-
-
   post '/figures' do
+    @figure = Figure.create(name: params[:figure][:name])
+
+    redirect to "/figures/#{@figure.id}"
   end
 
+
+
+
+  get '/figures/edit' do
+    @figure = Figure.find_by(params[:id])
+    erb :'figres/edit'
+  end
 end
