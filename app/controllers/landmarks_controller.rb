be0b0e242baf7ec9d0b3a62549c8f4d67fe0c36a
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-12 14:05:59 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,16 +1,17 @@
 class LandmarksController < ApplicationController
 
-  get '/landmarks' do
-
-    @landmarks = Landmark.all
-
-    erb :'/landmarks/index'
-  end
 
   get '/landmarks/new' do  #load a new page with loaded data
     @landmarks = Landmark.all
     erb :'/landmarks/new'
   end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+
 
   post '/landmarks' do #creates a new landmark
   #  binding.pry
