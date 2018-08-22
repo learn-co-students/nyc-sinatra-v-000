dd86c01bbe5baf1db0a62858356a0accc1b8bd20
pracniscate <lorena.giddens@outlook.com>
2018-08-22 12:34:51 -0400

    view form to edit a figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -34,4 +34,20 @@ class FiguresController < ApplicationController
         redirect to "/figures/#{@figure.id}"
     end
 
+    post '/figures/:id' do # update
+        @figure = Figure.find(params[:id])
+        @figure.update(params[:figure])
+
+        if !params[:landmark][:name].empty?
+            @figure.landmarks << Landmark.create(params[:landmark])
+        end
+
+        if !params[:title][:name].empty?
+            @figure.titles << Title.create(params[:title])
+        end
+
+        @figure.save
+        redirect to "/figures/#{@figure.id}"
+    end
+
 end
