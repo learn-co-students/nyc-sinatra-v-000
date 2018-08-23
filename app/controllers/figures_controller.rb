acff055d6ae6939f18de62c4a351128b06480106
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-23 21:21:03 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.create(name: params[:name])
+    redirect "/figures/#{@figure.id}"
+  end
+
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/show"
+  end
+end
