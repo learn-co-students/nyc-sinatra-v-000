1f1322cb88473578c5a1942f67513347439f9303
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-08-26 15:31:37 -0400

    Done.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,5 +1,13 @@
 class Figure < ActiveRecord::Base
   has_many :landmarks
-  has_many :figure_titles, :through => :figure_titles
-  has_many :figures, :through => :figure_titles
+  has_many :figure_titles
+  has_many :titles, :through => :figure_titles
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Figure.all.find {|figure| figure.slug == slug}
+  end
 end
--- db/migrate/01_create_landmarks_table.rb
+++ db/migrate/01_create_landmarks_table.rb
@@ -3,6 +3,8 @@ class CreateLandmarksTable < ActiveRecord::Migration
     create_table :landmarks do |t|
       t.string :name
       t.integer :year_completed
+
+      t.integer :figure_id
     end
   end
 end
