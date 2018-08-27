4d2764ec44ee88a7b52700a39e12c0abdc303288
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-08-26 20:16:49 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,6 +14,28 @@ class FiguresController < ApplicationController
     erb :'/figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'/figures/edit'
+  end
+
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
@@ -24,10 +46,13 @@ class FiguresController < ApplicationController
       @figure.landmarks << Landmark.create(params[:landmark])
     end
     @figure.save
+    redirect "/figures"
+  end
+
 
   # flash[:message] = "Successfully created figure."
      # redirect("/songs/#{@song.slug}")
-  end
+  # end
 
 
 
