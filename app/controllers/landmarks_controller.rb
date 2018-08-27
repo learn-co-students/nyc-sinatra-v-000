4cce0d71eca70f588707e45ba29ead866beb46a1
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-08-27 11:33:38 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,19 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmark/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmark/edit'
+  end
 end
