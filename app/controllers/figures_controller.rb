c1ea3030adfc3d722ffc77358fcff1ad7f9080bd
BrittanyTinnin <bntinnin7@gmail.com>
2018-08-28 17:14:51 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,31 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+
+    redirect to ('/figures/:id')
+  end
+
+  get '/figures/:id/edit' do
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    
+    redirect to ('/figures/:id')
+  end
 end
