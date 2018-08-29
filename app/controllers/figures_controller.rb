647d512f7122de5eea182aef9cea45ae978d62f3
github username <github email address>
2018-08-29 10:30:17 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,10 +14,20 @@ class FiguresController < ApplicationController
     post '/figures' do
         @figure = Figure.create(params["figure"])
         @figure.titles << Title.find_or_create_by(params["title"]) if params["title"]["name"].length >0
-        @figure.landmarks << Landmark.find_or_create_by(params["landmark"])if params["landmark"]["name"].length >0 && params["landmark"]["year"].length >0
+        @figure.landmarks << Landmark.find_or_create_by(params["landmark"])if params["landmark"]["name"].length >0 
         @figure.save
-      end
+        @figure
+    end
 
+    # get "/figures/#{@figure.id}" do
+
+    #     erb :'/figures/show'
+    # end
+
+    get '/figures/:id' do
+        @id = params[:id]
+        erb :figures
+      end
 
 
   
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/figures/index'
+  end
+
+  get '/landmark/new' do
+      @titles = Title.all
+      @figures = Figure.all
+    erb :'/landmarks/new'
+  end
+
 end
