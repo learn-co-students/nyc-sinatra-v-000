be98fe5f31426880d75edf95b75c4dad1c9ee1d3
Brent Bauman <brentbauman86@gmail.com>
2018-09-06 18:51:36 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
 
   get '/figures' do
     @figures = Figure.all
-    erb :"figures/index"
+    erb :"/figures/index"
   end
 
 
@@ -26,6 +26,11 @@ class FiguresController < ApplicationController
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
   end
 
 end
