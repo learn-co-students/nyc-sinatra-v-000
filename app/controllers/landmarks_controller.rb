24d30835bfa4ca98c8481e54894df1adad706b4a
treverandersen <trever.r.andersen@gmail.com>
2018-09-06 17:16:21 -0500

    allows you to edit a single landmark

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,6 +15,23 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/show'
   end
 
+  get '/landmarks/:id/edit' do 
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
 
+  post '/landmarks' do 
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect to("/landmarks")
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    
+    redirect to("/landmarks/#{@landmark.id}")
+  end
 
 end
