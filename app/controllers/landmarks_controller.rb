4feab4e8b764c661de96c1f69c73fbc93848d4c0
David McGrann <dpmcgrann@gmail.com>
2018-09-06 15:11:38 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -21,17 +21,17 @@ class LandmarksController < ApplicationController
     erb :'landmarks/edit'
   end
   
-  post '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    @landmark.name = params[:landmark][:name]
-    @landmark.year_completed = params[:landmark][:year_completed]
-    @landmark.save
-    redirect to "/landmarks/#{@landmark.id}"
-  end
   
   post '/landmarks' do
     Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
     redirect '/landmarks'
+  end
+  
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
   end
   
  
