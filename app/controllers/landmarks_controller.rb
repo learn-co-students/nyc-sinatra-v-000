247179647a2326b906fbd924535d10ded58328fe
Adam Duff <j.adam.duff@gmail.com>
2018-09-11 07:31:01 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,24 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/new'
   end
 
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+    redirect '/landmark/' + landmark.id
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
 end
