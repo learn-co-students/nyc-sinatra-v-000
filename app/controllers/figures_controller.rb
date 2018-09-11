a29cec7299e9fb658e56cf45f201e77db8813ca3
Adam Duff <j.adam.duff@gmail.com>
2018-09-11 07:09:57 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,58 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  post '/figures' do
+    figure = Figure.create(name: params[:figure]["name"])
+    if params[:figure].keys.include?("landmark_ids")
+      figure.landmarks << Landmark.find(params[:figure]["landmark_ids"].first)
+    elsif params[:figure].keys.include?("title_ids")
+      figure.titles << Title.find(params[:figure]["title_ids"].first)
+    elsif params[:landmark]["name"] != ""
+      landmark = Landmark.create(name: params[:landmark]["name"])
+      figure.landmarks << landmark
+    elsif params[:title]["name"] != ""
+      title = Title.create(name: params[:title]["name"])
+      figure.titles << title
+    end
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(name: params[:figure]["name"])
+    if params[:figure].keys.include?("landmark_ids")
+      figure.landmark_ids = params[:figure]["landmark_ids"]
+    elsif params[:figure].keys.include?("title_ids")
+      figure.titles_ids = params[:figure]["title_ids"]
+    elsif params[:landmark]["name"] != ""
+      landmark = Landmark.create(name: params[:landmark]["name"])
+      figure.landmarks << landmark
+    elsif params[:title]["name"] != ""
+      title = Title.create(name: params[:title]["name"])
+      figure.titles << title
+    end
+    redirect '/figures/' + figure.id
+  end
+
 end
