1febfc7109a717c110a765056719bf87674f78ef
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-12 03:49:18 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,4 +5,14 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure_name])
+    if params[:new_title]
+      @title = Title.create(name: params[:new_title])
+      @figure.title = @title
+    else
+      @figure.title = params[:title]
+    end
+  end
+
 end
