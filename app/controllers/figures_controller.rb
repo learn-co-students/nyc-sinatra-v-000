162a5962d5342dcb8f1b3dc6ee4146868e43f347
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-09-21 17:28:40 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,24 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'landmarks/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  
 end
