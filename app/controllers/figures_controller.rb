cfddef1310e6b3a27c8fc3ff6c06f4bb1ee15c91
Josely Castro <joselycas@gmail.com>
2018-10-05 10:33:11 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,37 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+     erb :index
+  end
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+      #binding.pry
+    @figure = Figure.create(params[:figure])
+  
+
+
+  end
+
+
+
+
 end
+
+
+#post '/songs' do
+  #  @song = Song.create(:name => params["Name"])
+  #  @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+  #  @song.genre_ids = params[:genres]
+  #  @song.save
+
+
+#{"figure"=>{"name"=>"Doctor Who", "title_ids"=>["231"]},
+#   "landmark"=>{"name"=>""},
+#   "title"=>{"name"=>""},
+#   "Create New Figure"=>"submit"}
