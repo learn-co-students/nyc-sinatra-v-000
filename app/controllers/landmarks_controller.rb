9c75aef36592a2fd072e9a9bb4fa97531c2db7fd
Nathaniel Shuster <nshu521992@gmail.com>
2018-10-06 01:38:21 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,16 @@
 class LandmarksController < ApplicationController
   
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+  
   get '/landmarks/new' do
     erb :'/landmarks/new'
   end
   
   post '/landmarks' do
-    
+    @landmark = Landmark.create(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
+    redirect '/landmarks'
   end
 end
