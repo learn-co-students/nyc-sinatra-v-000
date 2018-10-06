49a98aa5e30d885b60d0fefbbff60469d6f5095d
Nathaniel Shuster <nshu521992@gmail.com>
2018-10-06 01:31:45 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,10 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+  
+  post '/landmarks' do
+    
+  end
 end
