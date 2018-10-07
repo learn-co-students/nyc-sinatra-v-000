947d2dc135cb33db93b83c03c9a8a5bec114b52d
Josely Castro <joselycas@gmail.com>
2018-10-07 12:06:06 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,26 +3,49 @@ class FiguresController < ApplicationController
 
   get '/figures' do
     @figures = Figure.all
-     erb :index
+     erb :"figures/index"
   end
 
   get '/figures/new' do
     erb :"figures/new"
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
   post '/figures' do
-      #binding.pry
-    @figure = Figure.create(params[:figure])
-  
-
-
+   @figure = Figure.create(params[:figure])
+   if params[:title][:name] != ""
+    @figure.titles << Title.create(params[:title])
+   end
+   if params[:landmark][:name] != ""
+    @figure.landmarks << Landmark.create(params[:landmark])
+   end
+   redirect ("/figures/#{@figure.id}")
   end
 
 
+   get '/figures/:id/edit' do
+     @figure = Figure.find_by_id(params[:id])
+     erb :"figures/edit"
+   end
 
+   patch '/figures/:id' do
+     @figure = Figure.find_by_id(params[:id])
 
+     if params[:title][:name] != ""
+      @figure.titles << Title.create(params[:title])
+     end
+     if params[:landmark][:name] != ""
+      @figure.landmarks << Landmark.create(params[:landmark])
+     end
+     @figure.update(params[:figure])
+     @figure.save
+     redirect  ("/figures/#{@figure.id}")
+   end
 end
-
 
 #post '/songs' do
   #  @song = Song.create(:name => params["Name"])
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,22 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmark' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+   erb :"landmarks/new"
+ end
+
+ get '/landmarks/:id' do
+   @landmark = Landmark.find(params[:id])
+   erb :"landmarks/show"
+ end
+
+ post '/landmarks' do
+   @landmark = Landmark.create(params[:landmark])
+   redirect ("/landmarks/#{@landmark.id}")
+ end
+
 end
