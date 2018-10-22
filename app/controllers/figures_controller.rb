1ea4f9e8e9f36a2e390ef53fd2c321f0a9059161
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-22 09:12:38 -0700

    more work on fig views and controllers

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,4 +7,39 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'/figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    @figure.titles << Title.create(params[:title])
+
+    @figure.landmarks << Landmark.create(params[:landmark])
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    @figure.titles << Title.create(params[:title])
+    @figure.landmarks << Landmark.create(params[:landmark])
+    
+    @figure.save
+
+    redirect to :"/figures/#{@figure.id}"
+  end
+
 end
