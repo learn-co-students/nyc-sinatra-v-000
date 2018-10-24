d04a2e47ab39910150570cd26ec331f863ebfb68
amitnyc83 <amit_nyc@live.com>
2018-10-24 18:16:31 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,20 +1,32 @@
+require 'pry'
 class FiguresController < ApplicationController
   # add controller methods
 
   get "/figures" do
   @figures = Figure.all
-  
+
   erb :'/figures/index'
   end
 
   get '/figures/new' do
-    @titles = Title.all
-    @landmarks =Landmark.all
+  @titles = Title.all
+  @landmarks = Landmark.all
   erb :'/figures/new'
   end
 
-  post '/figures/new' do
-    @figure = Figure.create(params[:name])
-    erb :'figures/#{@figure.id}'
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.titles = Title.find_or_create_by(params[:title])
+    @figure.landmarks = Landmark.find_or_create_by(params[:landmark])
+
+    @figure.save
+
+    redirect to :"/figures/#{@figure.id}"
   end
 end
