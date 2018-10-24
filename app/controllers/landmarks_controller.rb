4be79a281ef5ff7f2d07b9cb296ca39c46d33d0c
Adam Barton <adam.barton333@gmail.com>
2018-10-24 19:37:35 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,19 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+   
+  end
+
 end
