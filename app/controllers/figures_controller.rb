43f43da92a6bdadac5902b469e921521be09d44c
amitnyc83 <amit_nyc@live.com>
2018-10-25 12:09:14 -0400

    show edit pages

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,11 +14,6 @@ class FiguresController < ApplicationController
   erb :'/figures/new'
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id])
-
-    erb :'/figures/show'
-  end
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
@@ -30,9 +25,27 @@ class FiguresController < ApplicationController
     redirect to :"/figures/#{@figure.id}"
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
   get '/figures/:id/edit' do
    @figure = Figure.find_by_id(params[:id])
 
    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    @figure.update(name: params[:figure][:name])
+    @figure.titles << Title.find_or_create_by(params[:title])
+    @figure.landmarks << Landmarks.find_or_create_by(params[:landmark])
+
+    @figure.save
+
+    redirect to '/figures/#{@figure.id}'
   end
 end
