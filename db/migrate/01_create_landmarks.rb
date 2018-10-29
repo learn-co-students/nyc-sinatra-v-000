f08d7166aee7a072c81501b35970b8d8773116db
Christia T <christiatidwell@bellsouth.net>
2018-10-29 21:02:20 +0000

    created figures, titles, and recreated landmarks tables

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
+
   create_table :landmarks do |t|
-  t.string :name
-  t.integer :figure_id
-  t.integer :year_completed
+    t.string :name
+    t.integer :figure_id
+    t.integer :year_completed
+
  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20181029205855) do
+
+  create_table "figures", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "landmarks", force: :cascade do |t|
     t.string  "name"
     t.integer "figure_id"
     t.integer "year_completed"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "name"
   end
 
 end
