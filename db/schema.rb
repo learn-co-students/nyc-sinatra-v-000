36e81a966c024c72b0ebc7ceba73a631314e0e53
Christia T <christiatidwell@bellsouth.net>
2018-10-29 21:08:39 +0000

    created figure_titles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029205855) do
+ActiveRecord::Schema.define(version: 20181029210335) do
+
+  create_table "figure_titles", force: :cascade do |t|
+    t.integer "title_id"
+    t.integer "figure_id"
+  end
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
