d7dc32ffb11ebfaeaf49b109e5c0a175f57528a8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-30 23:09:43 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,45 @@
+require 'pry'
 class FiguresController < ApplicationController
-  # add controller methods
+ get '/figures' do
+   @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+  
+   get '/figures/:id' do
+      @figures = Figure.find(params[:id])
+      erb :'figures/show'
+   end
+
+    get '/figures/:id/edit' do
+      @figures = Figure.find(params[:id])
+      erb :'figures/edit'
+    end
+  
+    post '/figures' do 
+      @figures = Figure.create(params['figure'])
+         if !params[:title][:name].empty? 
+          @figures.titles << Title.create(params[:name])
+         else !params[:landmark][:name].empty? 
+         @figures.landmarks << Landmark.create(params[:landmark])
+      end
+      @figures.save
+      erb :'/figures/show'
+    end
+   
+    patch '/figures/:id' do
+      @figures = Figure.find(params[:id])
+      @figures.update(params[:figure])
+      if !params[:title][:name].empty? 
+          @figures.title << Title.create(params[:name])
+         else !params[:landmark][:name].empty? 
+         @figures.landmark << Landmark.create(params[:landmark])
+      end
+      @figures.save
+        redirect "figures/#{@figures.id}"
+      end
+  
 end
