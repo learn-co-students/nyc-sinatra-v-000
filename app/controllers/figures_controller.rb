cdabba3f4b60153596fc10bb4d31f808a87aed15
Christia T <christiatidwell@bellsouth.net>
2018-10-31 03:26:28 +0000

    added patch route

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,15 @@
 class FiguresController < ApplicationController
   # add controller methods
 
-get '/figures' do
-  @figures = Figure.all
-  erb :'figures/index'
-end
-
-
-get '/figures/new' do
-  erb :'figures/new'
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
   end
+
+
+  get '/figures/new' do
+    erb :'figures/new'
+    end
 
 
   get '/figures/:id' do
@@ -33,7 +33,31 @@ get '/figures/new' do
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
+
     erb :'figures/edit'
+  end
+
+
+  patch '/figures/:id' do
+    if !params[:figure].keys.include?("title_ids")
+    params[:figure]["title_ids"] = []
+    end
+
+    if !params[:figure].keys.include?("landmark_ids")
+    params[:figure]["landmark_ids"] = []
+    end
+
+    @figure.update(params["figure"])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+
+    @figure.update(params["figure"])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+
+    redirect "figures/#{@figure.id}"
   end
 
 end
