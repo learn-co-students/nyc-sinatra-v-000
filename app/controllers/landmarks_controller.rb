5ac4087d18971c0fc1ac45e2e52a77fa7e4a951c
Christia T <christiatidwell@bellsouth.net>
2018-11-01 01:19:45 +0000

    added get, and post routes for landmark controller

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,24 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+    end
+
+    get '/landmarks/:id' do
+      @landmark = Landmark.find(params[:id])
+      erb :'landmarks/show'
+    end
+    
+    post '/landmarks' do
+      @landmark = Landmark.create(params["landmark"])
+      @landmark.save
+
+      redirect to "/figures/#{@landmark.id}"
+    end
+
 end
