211715f5546ded7b35b5ed990f432ec858b9f00a
AlwinaO <aoyewole2586@gmail.com>
2018-11-01 22:01:26 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,4 +3,22 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'/figures/new'
   end
+
+  post '/figures' do
+    # raise params.inspect
+    # {"figure"=>{"name"=>"New Figure", "title_ids"=>["2"], "landmark_ids"=>["9"]}, "title"=>{"name"=>""}, "landmark"=>{"name"=>""}
+    @figure = Figure.create(:figure => params["name"])
+    if !params["title"]["name"].empty? || !params["landmark"]["name"].empty?
+      @figure.title << Title.create(name: params["title"]["name"]) &&
+      @figure.landmark << Title.create(name: params["landmark"]["name"])
+      @figure.save
+    end
+
+    redirect to "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    erb :'figures/show'
+  end
+
 end
