6303e797bc596f0a7eb0db94f2f2f2c0ce3ef4de
AlwinaO <aoyewole2586@gmail.com>
2018-11-03 21:36:55 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,6 +12,11 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
   post '/figures' do
     # raise params.inspect
     # {"figure"=>{"name"=>"New Figure", "title_ids"=>["2"], "landmark_ids"=>["9"]}, "title"=>{"name"=>""}, "landmark"=>{"name"=>""}
@@ -29,10 +34,23 @@ class FiguresController < ApplicationController
     redirect to "/figures/#{@figure.id}"
   end
 
-  get '/figures/:id' do
-    binding.pry
+  get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
-    erb :'/figures/show'
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
   end
 
 end
