027c1f9a9b04a64d78406e9ceb994b1341c734c9
AlwinaO <aoyewole2586@gmail.com>
2018-11-04 19:57:25 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    # raise params.inspect
+    # {"landmark_name"=>"My House", "landmark_year_completed"=>"1981", "submit"=>"Create New Landmark"}
+
+    @landmark = Landmark.new
+    @landmark.name = params[:landmark_name]
+    @landmark.year_completed = params[:landmark_year_completed]
+    @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+
 end
