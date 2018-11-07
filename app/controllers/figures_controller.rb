5d8e86177d9cc7438d4122897e7164e94d0355ac
Brian Birdwell <bbirdwell@gmail.com>
2018-11-07 20:02:49 +0000

    complete figure controller and view for  /new and /show

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,5 @@
 class FiguresController < ApplicationController
+
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
@@ -8,15 +9,35 @@ class FiguresController < ApplicationController
   post '/figures' do
 
     @figure = Figure.create(params["figure"])
-    binding.pry
+    # binding.pry
     # Landmark.find_by(id: params["figure"]["landmark_ids"])
-    if params["title"]["name"]
+    if params["title"]["name"] != ""
       @title = Title.create(name: params["title"]["name"])
       @figure.titles << @title
-      @figure.save
     end
 
+    if params["landmark"]["name"] != ""
+      @landmark = Landmark.create(params["landmark"])
+      @figure.landmarks << @landmark
+    end
+    @figure.save
+    @figures = Figure.all
+    # erb :"figures/index"
+  end
+
+  get '/figures' do
     @figures = Figure.all
     erb :"figures/index"
+  end
+
+  get '/figures/:id' do
+    # binding.pry
+    @figure = Figure.find_by(id: params["id"])
+    # @titles = @figure.titles
+    erb :"figures/show"
+  end
+
+  get 'figures/:id/edit' do
+
   end
 end
