8875104e2ed11c8d72dd35341f19839d75844c95
Brian Birdwell <bbirdwell@gmail.com>
2018-11-07 21:11:25 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,34 @@
 class LandmarksController < ApplicationController
 
-  get '/figures/new' do
+  get '/landmarks/new' do
 
-    erb :"figures/new"
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    # binding.pry
+    @landmark = Landmark.create(params["landmark"])
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params["id"])
+    erb :"landmarks/edit"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params["id"])
+    erb :"landmarks/show"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params["id"])
+    @landmark.update(params["landmark"])
+
+    redirect "/landmarks/#{@landmark.id}"
   end
 end
