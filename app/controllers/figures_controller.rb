1369ee4d316344f5a6b4b18c9e54df297d9e9443
Brian Birdwell <bbirdwell@gmail.com>
2018-11-07 19:44:47 +0000

    add to figures_controller post method, figures/new view

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,22 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+
+    @figure = Figure.create(params["figure"])
+    binding.pry
+    # Landmark.find_by(id: params["figure"]["landmark_ids"])
+    if params["title"]["name"]
+      @title = Title.create(name: params["title"]["name"])
+      @figure.titles << @title
+      @figure.save
+    end
+
+    @figures = Figure.all
+    erb :"figures/index"
+  end
 end
