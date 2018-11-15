4419355c698b50d27574c93cd228a5e976bf6a2d
github username <github email address>
2018-11-14 19:56:03 -0700

    spec passing through /spec/figures_controller_view_spec.rb:109

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,36 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures'do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get  '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+
+    if !params[:title].blank?
+      title = Title.create(params[:title])
+      figure.titles << title
+      figure.save
+    end
+
+    if !params[:landmark][:name].blank?
+      landmark = Landmark.create(params[:landmark])
+      figure.landmarks << landmark
+      figure.save
+    end
+
+  end
+
 end
