014999af667efa90cb8b658f8166ffb79ade8325
Edsel Caprice <caprice@mail.usf.edu>
2018-11-29 01:01:49 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,10 +10,30 @@ class FiguresController < ApplicationController
      erb :'figures/new'
    end
 
+   get '/figures/:id' do
+     @figure = Figure.find_by_id(params[:id])
+     erb :'figures/show'
+   end
+
+   get '/figures/:id/edit' do
+     @figure = Figure.find_by_id(params[:id])
+     erb :'figures/edit'
+   end
+
    post '/figures' do
      @figure = Figure.create(params[:figure])
      @figure.titles << Title.create(params[:title])
      @figure.landmarks << Landmark.create(params[:landmark])
      @figure.save
+     redirect to "/figures/#{@figure.id}"
+   end
+
+   patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    @figure.titles << Title.create(params[:title])
+    @figure.landmarks << Landmark.create(params[:landmark])
+    @figure.save
+     redirect to "/figures/#{@figure.id}"
    end
 end
