72db8c7c0050f4b6f55f22cba92258cf2173c385
Marc Mills <mdmills1990@gmail.com>
2018-12-03 18:58:18 +0000

    adds post route and :id route to figures controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,4 +9,21 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
+  post '/figures' do
+    @figure = Figure.create(:name => params["Name"])
+    #some kind of logic that says if params of Title.find_or_create_by is "", then do the radio buttons
+    @figure.title_ids = params[:titles]
+    @figure.landmark_ids = params[:landmarks]
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    
+    erb :'figures/show'
+  end
+
+
+
 end
