0121ca5d1a4b900ca9032c7a90e4bd5a5ff74c91
Emily S <emily.sugano@gmail.com>
2018-12-09 23:23:36 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,2 @@
 class FiguresController < ApplicationController
-  # add controller methods
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,3 @@
 class Title < ActiveRecord::Base
-  # add relationships here
+  has_many :figures
 end
