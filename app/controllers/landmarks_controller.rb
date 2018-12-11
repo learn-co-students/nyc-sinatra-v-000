29deefc168011241131bc792b37cc7707c6e49a0
Barbara Choy <barbchoy@gmail.com>
2018-12-11 21:39:06 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,12 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.save
+    erb :'/landmarks/show'
+  end
 end
