8b182874da7c3152c39435bbebb9b943fe5008be
Barbara Choy <barbchoy@gmail.com>
2018-12-12 01:29:45 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,13 +1,19 @@
 class LandmarksController < ApplicationController
   # add controller methods
 
-  get '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
-    erb :'/landmarks/show'
-  end
 
   get '/landmarks/new' do
     erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
   end
 
   post '/landmarks' do
