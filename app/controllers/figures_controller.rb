9b65e547d60ea76c1310460ec41d1938c694ab87
Harley Rayner <harleymteden@gmail.com>
2018-12-14 21:59:56 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,27 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures/show' do
+    @figure = Figure.create(name: params[:figure][:name])
+    # erb :'/figures/show'
+  end
+
+  get '/figures/show' do
+    # @figure = Figure.find_by(name: params[:figure][:name])
+    erb :'/figures/show'
+  end
+
+
+
+
 end
