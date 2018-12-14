53a34483cf56f20691ac214d6eca306d04414986
Jonathan Luk <jluk87@gmail.com>
2018-12-14 03:22:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,63 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all.sort {|a,b| a.name <=> b.name}
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all.sort {|a,b| a.name <=> b.name}
+    @titles = Title.all.sort {|a,b| a.name <=> b.name}
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      new_title = Title.find_or_create_by(name: params[:title][:name])
+      if !@figure.titles.include?(new_title)
+        @figure.titles << new_title
+      end
+    end
+    if !params[:landmark][:name].empty?
+      new_landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+      if !@figure.landmarks.include?(new_landmark)
+        @figure.landmarks << new_landmark
+      end
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id])
+    @landmarks = Landmark.all.sort {|a,b| a.name <=> b.name}
+    @titles = Title.all.sort {|a,b| a.name <=> b.name}
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      new_title = Title.find_or_create_by(name: params[:title][:name])
+      if !@figure.titles.include?(new_title)
+        @figure.titles << new_title
+      end
+    end
+    if !params[:landmark][:name].empty?
+      new_landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+      if !@figure.landmarks.include?(new_landmark)
+        @figure.landmarks << new_landmark
+      end
+    end
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
 end
