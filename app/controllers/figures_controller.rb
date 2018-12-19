4892e41467f44ca77a20e9283de6ddbbe566253e
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-18 20:58:56 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -18,11 +18,37 @@ class FiguresController < ApplicationController
     @figure.titles << @title
     @figure.landmarks << @landmark
     @figure.save
-    redirect_to :"/figures/#{@figure.id}"
+    redirect "/figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :"/figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/figures/edit"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty? && !params[:landmark][:name].empty?
+      @title = Title.create(params[:title]) && @landmark = Landmark.create(params[:landmark])
+      @figure.titles << @title && @figure.landmarks << @landmark
+      @figure.save
+    elsif !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+      @figure.save
+    else !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+      @figure.save
+    end
+    redirect "/figures/#{@figure.id}"
   end
 end
