37e3e2c1714e614fcaa6ac1ef25ea8b3467577d3
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-20 23:13:32 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,74 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+
+    @figure = Figure.create(name: params[:figure][:name])
+
+    if params[:figure][:title_ids] != nil
+      @figure.titles = Title.find(params[:figure][:title_ids])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if params[:figure][:landmark_ids] != nil
+      @figure.landmarks = Landmark.find(params[:figure][:landmark_ids])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+    end
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    if !params[:figure][:name].empty?
+      @figure.name = params[:figure][:name]
+    end
+
+    if params[:figure][:title_ids] != nil
+      @figure.titles = Title.find(params[:figure][:title_ids])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if params[:figure][:landmark_ids] != nil
+      @figure.landmarks = Landmark.find(params[:figure][:landmark_ids])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+    end
+
+    @figure.save
+
+    erb :'/figures/show'
+
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,43 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    erb :'/landmark/show'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+
+    if !params[:landmark][:name].empty? && !params[:landmark][:year_completed].empty?
+      @landmark.name = params[:landmark][:name]
+      @landmark.year_completed = params[:landmark][:year_completed]
+    end
+
+    @landmark.save
+
+    erb :'/landmarks/show'
+
+  end
+
+
+
 end
