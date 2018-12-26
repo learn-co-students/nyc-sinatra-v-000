c11c8311c8b41098badae90219b7a7e11db984f3
John Werenski <jtwerenski@gmail.com>
2018-12-26 22:21:20 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,56 @@
 class FiguresController < ApplicationController
-  # add controller methods
-end
+  
+  get "/figures" do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end 
+  
+  get "/figures/new" do
+    @titles = Title.all 
+    @landmarks = Landmark.all 
+    erb :"/figures/new"
+  end 
+  
+  post "/figures" do
+    
+    if !Figure.find_by(name: params[:figure][:name])
+      @figure = Figure.create(params[:figure])
+      if !Title.all.find_by(name: params[:title][:name]) && !!params[:title][:name]
+          @figure.titles.create(params[:title])
+      end 
+      if !Landmark.find_by(name: params[:landmark][:name]) && !params[:landmark][:name].empty?
+          @figure.landmarks.create(params[:landmark])
+      end 
+      redirect :"/figures/#{@figure.id}"
+    else 
+      redirect "/figures/new"
+    end 
+  end 
+  
+  get "/figures/:id" do 
+   @figure = Figure.find(params[:id])
+   erb :"/figures/show"
+  end 
+  
+  get "/figures/:id/edit" do 
+   @figure = Figure.find(params[:id])
+   @landmarks = Landmark.all 
+   @titles = Title.all 
+   erb :"/figures/edit"
+  end 
+  
+  patch "/figures/:id" do 
+    @figure = Figure.find(params[:id])
+    #binding.pry
+    @figure.update(params[:figure])
+    if !Title.all.find_by(name: params[:title][:name]) && !params[:title][:name].empty?
+        @figure.titles.create(params[:title])
+    end 
+    if !Landmark.find_by(name: params[:landmark][:name]) && !params[:landmark][:name].empty?
+        @figure.landmarks.create(params[:landmark])
+    end 
+   
+    redirect :"/figures/#{@figure.id}"
+  end 
+  
+  end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+   
+   get "/landmarks" do
+     @landmarks = Landmark.all
+     erb :"/landmarks/index"
+   end 
+   
+   get "/landmarks/new" do 
+     erb :"/landmarks/new"
+   end 
+   
+   post "/landmarks" do 
+     if !Landmark.find_by(name: params[:landmark][:name]) && !params[:landmark][:name].empty?
+        @landmark = Landmark.create(params[:landmark])
+        redirect :"/landmarks/#{@landmark.id}"
+     else
+       redirect :"/landmarks/new"
+     end 
+   end 
+   
+   get "/landmarks/:id" do 
+     @landmark = Landmark.find(params[:id])
+     erb :"/landmarks/show"
+   end 
+   
+   get "/landmarks/:id/edit" do 
+     @landmark = Landmark.find(params[:id])
+     erb :"/landmarks/edit"
+   end 
+   
+   patch "/landmarks/:id" do 
+     @landmark = Landmark.find(params[:id])
+     @landmark.update(params[:landmark])
+     redirect :"/landmarks/#{@landmark.id}"
+   end 
+   
 end
