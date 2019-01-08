04e4f3034aa3ab8f7200f753d19b7681706abffe
Emily S <emily.sugano@gmail.com>
2019-01-08 23:48:52 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,18 +5,31 @@ class FiguresController < ApplicationController
     erb :"figures/index"
   end
 
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :"figures/show"
   end
 
-  get '/figures/new' do
-    erb :"figures/new"
-  end
-
   post '/figures' do
+    binding.pry
+    # Figure
     @figure = Figure.create(:name => params[:figure][:name])
-    @title = Title.create(:name => params[:title][:name])
+    #Titles
+    @titles = []
+    #Adding new titles
+    if !params[:titles][:name].nil?
+      @titles << Title.create(:name => params[:titles][:name])
+    end
+    #Adding existing titles
+    if params[:titles][:title_ids] != []
+      params[:titles][:title_ids].each do |id|
+        @titles << Title.find_by(id)
+      end
+    end
     @landmark = Landmark.create(:name => params[:landmark][:name])
     @landmark.figure = @figure
     @figure.titles << @title
