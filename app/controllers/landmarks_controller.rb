4ab49a562099ce5e4bb967ff4d76ed8b6da6bad9
Vince Andrew Pimentel <contact@vincep.me>
2019-01-15 00:52:02 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,34 @@ class LandmarksController < ApplicationController
 
     erb :"/landmarks/index"
   end
+
+  get "/landmarks/new" do
+    erb :"/landmarks/new"
+  end
+
+  post "/landmarks" do
+    landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+
+    erb :"/landmarks/show"
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+
+    erb :"/landmarks/edit"
+  end
+
+  patch "/landmarks/:id" do
+    landmark = Landmark.find(params[:id])
+
+    landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+
+    redirect "/landmarks/#{landmark.id}"
+  end
 end
