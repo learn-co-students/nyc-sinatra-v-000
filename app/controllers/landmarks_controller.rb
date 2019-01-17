102cb244f4a9f58d0ab5d83209af69ce52d01410
Emily S <emily.sugano@gmail.com>
2019-01-17 21:14:46 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,11 +11,27 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    binding.pry
+    Landmark.create(:name => params[:landmark][:name], year_completed:params[:landmark][:year_completed])
   end
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/show"
   end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = landmark.find_by_id(params[:id])
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
+    @landmark.titles << Title.create(:name => params[:title][:name])  if !params[:title][:name].empty?
+    @landmark.figure << Figure.create(:name => params[:figure][:name])  if !params[:figure][:name].empty?
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
