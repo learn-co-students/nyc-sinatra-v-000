e0ee3586e3b83781c47dc305cf729af716b1438a
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-17 00:37:54 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,6 +12,40 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
+
+    @landmark.figure_id = params[:landmark][:figure_id]
+    if !params[:figure][:name].empty?
+      @landmark.figure = Figure.create(params[:figure])
+    end
+
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @figures = Figure.all.sort { |a, b| a.name <=> b.name }
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+
+    @landmark.figure_id = params[:landmark][:figure_id]
+    if !params[:figure][:name].empty?
+      @landmark.figure = Figure.create(params[:figure])
+    end
+
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
   end
 
 end
