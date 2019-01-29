e0b22ba46eebd6b98e9e20c7b5c12e36238bb818
Heather Miller <heather.miller114@gmail.com>
2019-01-29 18:43:40 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,12 +2,17 @@ class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
     #list all figures
+    @figures = Figure.all
     erb :'figures/index'
   end
 
   get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
     #create new figures
     erb :'figures/new'
+
+    redirect '/figures/:id'
   end
 
   post '/figures' do
