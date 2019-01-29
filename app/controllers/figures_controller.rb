5acf6f6e58668d9bf0ae0376fc79445a29e885cb
Heather Miller <heather.miller114@gmail.com>
2019-01-29 16:21:59 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    #list all figures
+  end
+
+  get '/figures/new' do
+    #create new figures
+  end
+
+  post '/figures' do
+  end
+
+  get '/figures/:id' do
+    #see a single figure
+  end
+
+  get '/figures/:id/edit' do
+    #edit a single figure
+  end
+
+  patch '/figures/:id' do
+    
+  end
+
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,29 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  
+  get '/landmarks' do
+    #list all landmarks
+  end
+
+  get '/landmarks/new' do
+    #create a new landmark
+  end
+
+  post '/landmarks' do
+
+  end
+
+  get '/landmarks/:id' do
+    #view single landmark
+  end
+
+  get '/landmarks/:id/edit' do
+    #edit single landmark
+  end
+
+  patch '/landmarks/:id' do
+    
+  end
+
+
 end
