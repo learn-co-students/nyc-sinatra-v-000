08a2cebe2f74849a1bd1783ab1b526c659281c95
Heather Miller <heather.miller114@gmail.com>
2019-01-29 16:26:27 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,10 +2,12 @@ class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
     #list all figures
+    erb :'figures/index'
   end
 
   get '/figures/new' do
     #create new figures
+    erb :'figures/new'
   end
 
   post '/figures' do
@@ -13,14 +15,16 @@ class FiguresController < ApplicationController
 
   get '/figures/:id' do
     #see a single figure
+    erb :'figures/show'
   end
 
   get '/figures/:id/edit' do
     #edit a single figure
+    erb :'figures/edit'
   end
 
   patch '/figures/:id' do
-    
+
   end
 
 
