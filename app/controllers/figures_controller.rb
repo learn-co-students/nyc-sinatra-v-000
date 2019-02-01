ea032f8ecc089060f1742ece339555df0ff5bf98
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-31 20:00:52 -0500

    Stub out controllers

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+    redirect "/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id/edit' do
+
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmark/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmark/show'
+  end
+
+  get '/landmark/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'/landmark/new'
+  end
+
+  post '/landmark' do
+    @landmark = Landmark.create(params)
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmark/:id/edit' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmark/edit'
+  end
+
+  patch '/landmark/:id' do
+    
+  end
+
 end
