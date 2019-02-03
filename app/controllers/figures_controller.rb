344f374b95bb14eed20a940ed44ffc2c8fdd7c38
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-02 20:27:30 -0500

    Edit figures controller
    Fixed find method in controller to find figures by id.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,11 +5,6 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find_by(params[:id])
-    erb :'/figures/show'
-  end
-
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
@@ -17,12 +12,44 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(params)
-    redirect "/figures/#{@figure.id}"
+    @figure = Figure.create(name: params[:figure][:name])
+    # binding.pry
+    @figure.titles << Title.find(params[:figure][:title_ids]) unless !params[:figure][:title_ids]
+    @figure.titles << Title.create(name: params[:title][:name]) unless params[:title][:name] == ""
+    
+    @figure.landmarks << Landmark.find(params[:figure][:landmark_ids]) unless !params[:figure][:landmark_ids]
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed]) if params[:landmark][:name] != ""
+    
+    @figure.save
+    redirect "figures/#{@figure.id}"
   end
 
-  patch '/figures/:id/edit' do
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(name: params[:figure][:name])
+
+    @figure.titles.clear
+    @figure.titles << Title.find(params[:figure][:title_ids]) unless !params[:figure][:title_ids]
+    @figure.titles << Title.create(name: params[:title][:name]) unless params[:title][:name] == ""
+    
+    @figure.landmarks.clear
+    @figure.landmarks << Landmark.find(params[:figure][:landmark_ids]) unless !params[:figure][:landmark_ids]
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed]) if params[:landmark][:name] != ""
+    
+    @figure.save
+    redirect "figures/#{@figure.id}"
   end
 
 end
