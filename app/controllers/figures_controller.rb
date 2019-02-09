19ae8da8c7d9784b230cf5afa549abab5c07e159
Jason Mei <jason.mei403@gmail.com>
2019-02-09 06:09:54 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,7 +10,6 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
-    @figure.title = Title.find_or_create_by(params[:title])
     binding.pry
     @figure.save
     redirect "/figures/#{@figure.id}"
