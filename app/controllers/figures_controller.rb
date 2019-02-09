62d3242d26fcb2938ab2fa5f6ce37e4f920af774
Jason Mei <jason.mei403@gmail.com>
2019-02-09 04:01:35 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,23 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.title = Title.find_or_create_by(params[:title])
+    binding.pry
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    erb :'/figures/show'
+  end
 end
