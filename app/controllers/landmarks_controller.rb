fa8f956d1b6bf9feb2636613344798ce2af6394e
Jason Mei <jason.mei403@gmail.com>
2019-02-13 00:23:24 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,22 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.figure = Figure.find_or_create_by(params[:figure]) if !params[:figure][:name].empty?
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(:id)
+    binding.pry
+    erb :'/landmarks/show'
+  end
 end
