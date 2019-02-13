0d36eb806e1219223ef3b4d6395eca4153001c94
Jason Mei <jason.mei403@gmail.com>
2019-02-13 01:04:59 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,14 +9,22 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
-    @landmark.figure = Figure.find_or_create_by(params[:figure]) if !params[:figure][:name].empty?
-    @landmark.save
     redirect "/landmarks/#{@landmark.id}"
   end
 
   get '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(:id)
-    binding.pry
+    @landmark = Landmark.find_by_id(params[:id])
     erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
   end
 end
