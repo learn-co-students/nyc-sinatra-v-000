912bc3bd0a2d3f4a42f0133e4bd90ef304c0dfbd
anihakutin <4hbrody@gmail.com>
2019-02-17 00:21:22 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,43 @@
 class FiguresController < ApplicationController
 
-  get '/figures/new' do
-    
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
   end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    # binding.pry
+   #  {"figure"=>{"name"=>"Donald Trump", "title_ids"=>["3"], "landmark_ids"=>["1"]},
+   # "title"=>{"name"=>"Your Fired!"},
+   # "landmark"=>{"name"=>"Trump Tower", "year"=>"1974"},
+   # "submit"=>"Create New Figure"}
+   @figure = Figure.create(params[:figure])
+   @figure.titles << Title.create(params[:title])
+   @figure.landmarks << Landmark.create(name: params[:landmark][:new_landmark], year_completed: params[:landmark][:year_completed])
+   @figure.save
+
+   redirect :"/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+
+  end
+
+  patch '/figures' do
+
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
 class LandmarksController < ApplicationController
-  
+
+  get '/figures/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/figures' do
+
+  end
+
+  get 'figures/:id' do
+    
+  end
 end
