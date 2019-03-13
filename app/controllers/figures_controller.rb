fa886878fa107cf44f44855404fe65edf6bb4b26
Ashley Wong <ashwong04@gmail.com>
2019-03-13 18:22:59 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,6 +4,16 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  post '/figures' do
+    @figure = Figure.find_or_create_by(:name => params[:figure][:name])
+    if !params[:figure][:title_ids].empty?
+      @figure.title_ids = params[:figure][:title_ids]
+    else
+      @figure.title_ids = Title.create(params[:title])
+    end
+    @figure.save
+    redirect '/figures/#{slug}'
+  end
 
 
 end
