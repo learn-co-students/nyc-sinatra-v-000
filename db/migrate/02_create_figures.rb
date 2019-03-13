8c02ef30cbac3bf7003d1bec9077c4746cabaa88
Ashley Wong <ashwong04@gmail.com>
2019-03-13 13:48:07 +0000

    Done.

--- db/migrate/02_create_figures.rb
+++ db/migrate/02_create_figures.rb
@@ -3,6 +3,8 @@ class CreateFigures < ActiveRecord::Migration
   def change
     create_table :figures do |t|
       t.string :name
+      t.integer :landmark_id
+      t.integer :figure_titles
     end
   end
 end
