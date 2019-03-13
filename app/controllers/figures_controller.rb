340f00a496021a5b9eca4acbe1c2bd91c370475b
Ashley Wong <ashwong04@gmail.com>
2019-03-13 21:54:28 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,10 +9,22 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    # binding.pry
     @figure = Figure.find_or_create_by(params[:figure])
-
+    @figure.title_ids = params[:figure][:title_ids]
+    @figure.landmark_ids = params[:figure][:landmark_ids]
+    
+    @figure.save
     redirect '/figures/#{@figure.slug}'
   end
+  
+#   {"figure"=>{"name"=>"cat", "title_ids"=>["1"], "landmark_ids"=>["19"]},
+# "title"=>{"name"=>""},
+# "landmark"=>{"name"=>"", "year"=>""}}
+  
+  
+  
+  
 
   get '/figures/:slug' do
     @figure = Figure.find_by_slug(params[:slug])
