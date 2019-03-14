2448e3c7e2743b1da89437badcdacd8ccdd85731
Ashley Wong <ashwong04@gmail.com>
2019-03-14 01:35:25 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,22 +9,24 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    # binding.pry
     @figure = Figure.find_or_create_by(:name => params[:figure][:name])
     @figure.titles = Title.where(id: params[:figure][:title_ids])
+    if !params[:title].empty?
+      @figure.titles << Title.create(params[:title])
+    end
     @figure.landmarks = Landmark.where(id: params[:figure][:landmark_ids])
+    if !params[:landmark].empty?
+          binding.pry
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
 
     @figure.save
     redirect "/figures/#{@figure.slug}"
   end
-# Figure.find_or_create_by(:name => params[:figure][:name])
-#   {"figure"=>{"name"=>"cat", "title_ids"=>["1"], "landmark_ids"=>["19"]},
-# "title"=>{"name"=>""},
-# "landmark"=>{"name"=>"", "year"=>""}}
 
-
-
-
+  # {"figure"=>{"name"=>"Doctor Who", "title_ids"=>["63"]},
+  #  "title"=>{"name"=>""},
+  #  "landmark"=>{"name"=>"", "year"=>""}}
 
   get '/figures/:slug' do
     @figure = Figure.find_by_slug(params[:slug])
