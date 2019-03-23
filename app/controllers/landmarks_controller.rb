8705f60c8c939d3beef5053d53f88a9b79b18a16
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-23 15:25:17 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get "/landmarks/new" do
+    @landmarks = Figure.all
+    erb :'/landmarks/new'
+  end
+
+  get "/landmarks/:slug" do
+    @landmark = Landmark.find_by_slug(params[:slug])
+    @figure = @landmark.figure
+    @titles = Title.all
+    erb :'/landmarks/show'
+  end
+
+  get "/landmarks" do
+    
+  end
+
+  post "/landmarks" do
+  end
+
+  get "/landmarks/:slug/edit" do
+  end
+
+  patch "/landmarks/:slug" do
+  end
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -2,4 +2,20 @@ class Title < ActiveRecord::Base
   # add relationships here
   has_many :figure_titles
   has_many :figures, through: :figure_titles
+
+  def slug
+    slug = self.name.downcase
+    slug = slug.split
+    slug_array
+    slug.map! do |word|
+      slug_array << word.gsub(/\W/,"")
+    end
+    slug_array = slug_array.join("-")
+    slug = slug_array
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    Title.all.find{|title| title.slug == slug}
+  end
 end
