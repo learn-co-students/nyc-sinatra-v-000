5c2164b894e8117b92eff97b9a1443e912d4e97b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-25 16:34:10 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -13,7 +13,7 @@ class LandmarksController < ApplicationController
   end
 
   get "/landmarks" do
-    #remember the route isn't     its this ^ don't forget the seeds
+    #remember the route isn't     its this ^, also don't forget the seeds
     @landmarks = Landmark.all
     erb :'/landmarks/index' #this
   end
