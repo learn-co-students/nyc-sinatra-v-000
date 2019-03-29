f9bdcde9aa02fae4f94b142be7662228653a3b52
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-29 16:31:34 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get "/figures" do
+    erb :'/figures/index'
+  end
+
+  get "/figures/new" do
+    erb :'/figures/new'
+  end
+
+  get "/figures/:id" do
+    erb :'/figures/show'
+  end
+
+  post "/figure" do
+    redirect to("/landmarks/#{@landmark.id}")
+  end
+
+  get "/figure/:id/edit" do
+    erb :"/landmarks/edit"
+  end
+
+  patch "/figure/:id" do
+    redirect to("/landmarks/#{@landmark.id}")
+  end
 end
