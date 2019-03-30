feb02942a17294463636fbfe41fe03d7de85fffe
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-30 18:37:56 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,14 +14,6 @@ class FiguresController < ApplicationController
   end
 
   post "/figures" do
-    if !params[:title].empty? && !Title.find_by(name: params["title"]["name"]) || !Landmark.find_by(year_completed: params["landmark"]["year_completed"])
-      @title = Title.create(params[:title])
-      @landmark.save
-    #else params[:title].empty?
-    #  @landmark = Title.find_by_id(params["Title"]["figure_id"])
-    #  @landmark.save
-    end
-
     if !params[:landmark].empty? && !Landmark.find_by(name: params["landmark"]["name"]) || !Landmark.find_by(year_completed: params["landmark"]["year_completed"])
       @landmark = Landmark.create(params[:landmark])
       @landmark.save
@@ -32,15 +24,24 @@ class FiguresController < ApplicationController
     #  @landmark.save
     end
 
+    if !params[:title].empty? && !Title.find_by(name: params["title"]["name"]) || !Landmark.find_by(year_completed: params["landmark"]["year_completed"])
+      @title = Title.create(params[:title])
+      @title.save
+    else params["title"]["name"].empty?
+      binding.pry
+      @title = Title.find_by_id(params["figure"]["title_ids"].first)
+      @title.save
+    end
+
     if !params["figure"]["name"].empty? && !Figure.find_by(name: params["figure"]["name"])
       @figure = Figure.create(name: params["figure"]["name"])
       @landmark.figure = @figure
       @landmark.save
-    else params["figure"]["name"].empty?
-      @figure = Figure.find_by_id(params["landmark"]["figure_id"])
-      @landmark.figure_id = params["landmark"]["figure_id"]
-      @landmark.figure = @figure
-      @landmark.save
+    #else params["figure"]["name"].empty?
+    #  @figure = Figure.find_by_id(params["landmark"]["figure_id"])
+    #  @landmark.figure_id = params["landmark"]["figure_id"]
+    #  @landmark.figure = @figure
+    #  @landmark.save
     end
     redirect to("/figures/#{@figure.id}")
   end
