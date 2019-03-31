c2acc34c413b0ea7c203f9516160695e465b389c
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-31 20:09:43 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,35 +16,38 @@ class FiguresController < ApplicationController
   post "/figures" do
     @figure = Figure.create(params[:figure]) #no if else needed because there is no checklist for figure.
     #New and Edit work on a singular figure.
+    #Failure/Error: check "title_#{Title.first.id}" means change input id to include erb tags to implement ruby
 
-    if !(params[:landmark].empty?) && !(Landmark.find_by(name: params["figure"]["landmark_ids"][])) # || !Landmark.find_by(year_completed: params["landmark"]["year_completed"])
-      @landmark = Landmark.create(params[:landmark])
-      @figure.landmarks << @landmark
-    elsif params["figure"]["landmark_ids"].empty?
-     #params["figure"]["landmark_ids"].each do |id|
-     @landmark = Landmark.find_by()
-     @landmark.save
-    end
 
     #if !params[:title].empty? && !Title.find_by(name: params["title"]["name"])
 
     #  @title = Title.create(params[:title])
     #  @title.save
-    if !(params[:title][:name].empty?) && !(Title.find_by(id: params["figure"]["title_ids"][])) #parenthesis helps alot
+    if !(params[:title][:name].empty?) #&& !(Title.find_by(id: params["figure"]["title_ids"][])) #parenthesis helps alot
       @title = Title.create(params[:title])
       @figure.titles << @title #many to many, more than one title
       @figure.save
-    elsif Title.find_by(id: params["figure"]["title_ids"].first)
-      params["figure"]["title_ids"].each do |id|
-        binding.pry
-        @title = Title.find_by(id: id)
-        @figure.titles << @title #many to many, more than one title
-        @figure.save
-      end
+    #elsif Title.find_by(id: params["figure"]["title_ids"].first)
+    #  params["figure"]["title_ids"].each do |id|
+    #    @title = Title.find_by(id: id)
+    #    @figure.titles << @title #many to many, more than one title
+    #    @figure.save
+    #  end
     else
     end
     #extra else stuff not needed for title
-
+  #  if !(params[:landmark].empty?) #&& !(Landmark.find_by(name: params["figure"]["landmark_ids"][])) # || !Landmark.find_by(year_completed: params["landmark"]["year_completed"])
+  #    @landmark = Landmark.create(params[:landmark])
+  #    @figure.landmarks << @landmark
+  #    @figure.save
+  #  elsif params["figure"]["landmark_ids"].empty?
+  #   params["figure"]["landmark_ids"].each do |id|
+  #     @landmark = Landmark.find_by(id: id)
+  #     @figure.landmarks << @landmark
+  #     @figure.save
+  #   end
+  #  else
+  #  end
     redirect to("/figures/#{@figure.id}")
   end
 
