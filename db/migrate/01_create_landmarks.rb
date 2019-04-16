25d5cadd70e51d4080630355fe588864db3dec53
Cheryl K <cherylkelson@icloud.com>
2019-04-16 13:31:57 +0000

    remove t.integer as id from createlandmarks in db/migrate

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,7 +2,6 @@ class CreateLandmarks < ActiveRecord::Migration
   # raise 'Write CreateLandmarks migration here'
   def change
     create_table :landmarks do |t|
-      t.integer :id
       t.string :name
       t.integer :figure_id
       t.integer :year_completed
