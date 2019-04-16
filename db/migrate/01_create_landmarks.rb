b96bda58f98b835bd3517538c0540931a0d9a930
Cheryl K <cherylkelson@icloud.com>
2019-04-16 13:20:51 +0000

    create table for landmarks

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  # raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :landmarks do |t|
+      t.integer :id
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
 end
