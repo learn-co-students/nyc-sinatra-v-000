33ae8ee430be0becd93a46c009491933612bcdd5
Christiana Vandermale <christianavdm@gmail.com>
2019-06-05 18:15:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -38,8 +38,19 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.landmarks.clear
+    @figure.titles.clear
 
-    binding.pry
+    params[:figure][:landmark_ids].each do |landmark_id|
+      landmark = Landmark.find(landmark_id)
+      @figure.landmarks << landmark
+    end
+
+    params[:figure][:title_ids].each do |title_id|
+      title = Title.find(title_id)
+      @figure.titles << title
+    end
 
     if !params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
@@ -48,6 +59,8 @@ class FiguresController < ApplicationController
     if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
+
+    @figure.save
 
     redirect "figures/#{@figure.id}"
   end
