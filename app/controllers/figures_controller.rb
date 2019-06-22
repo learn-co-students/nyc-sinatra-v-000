48f42650a4068cae4eeac83edd7585d3716d5445
BusterStatus <matthew.serre@gmail.com>
2019-06-22 16:32:38 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,37 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    title = Title.create(params[:title])
+    @figure.titles << title
+    landmark = Landmark.create(params[:landmark])
+    @figure.landmarks << landmark
+    @figure.figure_titles.create(title: title)
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:slug/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures' do
+
+  end
 end
