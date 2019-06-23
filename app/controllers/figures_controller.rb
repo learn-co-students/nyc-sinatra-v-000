85782e57ba24c3b02fc68a00373c9ce89ee46f80
BusterStatus <matthew.serre@gmail.com>
2019-06-22 17:39:52 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,16 +14,25 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params[:figure])
     title_ids = params[:figure]["title_ids"]
-    title_ids.each do |id|
-      title = Title.find(id)
-      @figure.titles << title
-      @figure.figure_titles.create(title: title)
+    if !title_ids.nil?
+      title_ids.each do |id|
+        title = Title.find(id)
+        @figure.titles << title
+        @figure.figure_titles.create(title: title)
+      end
     end
     title = Title.find_or_create_by(name: params[:title]["name"])
     @figure.titles << title
-    landmark = Landmark.create(params[:landmark])
-    @figure.landmarks << landmark
     @figure.figure_titles.create(title: title)
+    landmark_ids = params[:figure]["landmark_ids"]
+    if !landmark_ids.nil?
+      landmark_ids.each do |id|
+        landmark = Landmark.find(id)
+        @figure.landmarks << landmark
+      end
+    end
+    landmark = Landmark.find_or_create_by(name: params[:landmark]["name"])
+    @figure.landmarks << landmark
     redirect "/figures/#{@figure.id}"
   end
 
