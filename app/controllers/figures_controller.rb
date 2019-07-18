cdb4adc7da7dba08b3b974f70e5f9b00869d64d0
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-18 18:28:20 +0000

    /figures/new allows creation of new figures with titles and/or landmarks

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,27 +16,29 @@ class FiguresController < ApplicationController
   end
 
   post "/figures" do
-    @figure = Figure.create(params)
+    # binding.pry
+    @figure = Figure.create(params[:figure])
+    @landmark = Landmark.create(params[:landmark])
     redirect "/figures/#{@figure.id}"
   end
-
-  get "/figures/#{@figure.id}" do
-    @figure = Figure.find_by_id(params[:id])
-    erb :"figures/show"
-  end
-
-  get "/figures/#{@figure.id}/edit" do
-    @figure = Figure.find_by_id(params[:id])
-    erb :"figures/edit"
-  end
-
-  patch "/figures/#{@figure.id}" do
-    @figure = Figure.find_by_id(params[:id])
-    @figure.name = params[:figure_name]
-    @figure.landmark = params[:new_landmark]
-    @figure.save
-    redirect "/figures/#{@figure.id}"
-  end
+  #
+  # get "/figures/#{@figure.id}" do
+  #   @figure = Figure.find_by_id(params[:id])
+  #   erb :"figures/show"
+  # end
+  #
+  # get "/figures/#{@figure.id}/edit" do
+  #   @figure = Figure.find_by_id(params[:id])
+  #   erb :"figures/edit"
+  # end
+  #
+  # patch "/figures/#{@figure.id}" do
+  #   @figure = Figure.find_by_id(params[:id])
+  #   @figure.name = params[:figure_name]
+  #   @figure.landmark = params[:new_landmark]
+  #   @figure.save
+  #   redirect "/figures/#{@figure.id}"
+  # end
 
 
 end
