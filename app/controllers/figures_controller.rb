6f47e2b889a832b879568125e507515bb8685ba6
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-18 17:21:03 +0000

    Creates & Updates dbs, models, and first controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,42 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  #from Specs
+  # visit '/figures'
+  # visit '/figures/new'
+  # get "/figures/#{@figure.id}"
+  # get "/figures/#{@figure.id}/edit"
+
+
+
+  get "/figures" do
+    erb :"/figures/index"
+  end
+
+  get "/figures/new" do
+    erb :"/figures/new"
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params)
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get "/figures/#{@figure.id}" do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/show"
+  end
+
+  get "/figures/#{@figure.id}/edit" do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/edit"
+  end
+
+  patch "/figures/#{@figure.id}" do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure_name]
+    @figure.landmark = params[:new_landmark]
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+
 end
