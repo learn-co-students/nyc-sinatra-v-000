7bfebf3dfe91fc934cbb003892ee3416b3576622
Amil Masic <amil.masic@gmail.com>
2019-08-13 11:09:24 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,4 +4,33 @@ class FiguresController < ApplicationController
 
     erb :'/figures/index'
   end
+
+  get '/figures/new' do
+    # @titles = Title.all
+    # @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    # create a new figure
+    # binding.pry
+    @figure = Figure.create(params['figure'])
+    # with existing title
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    # with new title
+
+    # with existing landmark
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    # with new landmark
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    erb :'figures/show'
+  end
 end
