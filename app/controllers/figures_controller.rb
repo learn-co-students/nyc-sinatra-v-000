bf0d1bc9bc7861c584b0cf1d4f7aee6dbddd7bc0
Sdcrouse <sdcrouse91@gmail.com>
2019-08-21 22:21:49 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,7 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :"figures/new"
+  end
 end
