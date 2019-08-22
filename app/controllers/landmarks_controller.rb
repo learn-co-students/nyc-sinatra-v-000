e60c5b47acc5efe16a1cddb53bff28b1738c2177
Sdcrouse <sdcrouse91@gmail.com>
2019-08-21 17:28:11 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,26 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+  
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
-
+  
   post '/landmarks' do
-    "The new landmark has been created!"
+    landmark = Landmark.new(params[:landmark])
+    landmark.save unless Landmark.all.detect{|l| l.name == landmark.name}
+    "The landmark has been built!"
+  end
+  
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/edit"
   end
 end
