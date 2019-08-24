63289429225360853657471eac32f55e16baf0ed
Sdcrouse <sdcrouse91@gmail.com>
2019-08-23 20:48:13 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,4 +33,25 @@ class FiguresController < ApplicationController
 
     erb :"figures/edit"
   end
+
+  patch '/figures/:id' do
+    ####### Double bug fix - gets rid of existing titles and landmarks (There should be a way to refactor this, but how?)
+    if params[:figure][:title_ids].nil?
+      params[:figure][:title_ids] = []
+    end
+
+    if params[:figure][:landmark_ids].nil?
+      params[:figure][:landmark_ids] = []
+    end
+    ####### End of double bug fix
+
+    figure = Figure.find_by_id(params[:id])
+
+    figure.update(params[:figure])
+    figure.titles.build(params[:title]) unless params[:title][:name].blank?
+    figure.landmarks.build(params[:landmark]) unless params[:landmark][:name].blank?
+    figure.save
+
+    redirect to "/figures/#{params[:id]}"
+  end
 end
