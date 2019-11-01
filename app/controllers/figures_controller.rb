a3420a21254d421db23155be5fa9e3e8b0616c88
Diapause <diapause@protonmail.com>
2019-10-31 20:23:08 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,14 +14,47 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    figure = Figure.find_or_create_by(params["figure"])
-    binding.pry
-    figure.titles << Title.find_or_create_by(params["title"])
-    figure.landmarks << Landmark.find_or_create_by(params["landmark"])
-    figure.save
+    figure = Figure.find_by(name: params["figure"]["name"])
 
-    redirect :"/figures/#{"whatever"}"
+    if figure
+      redirect :"/figures/#{figure.id}/edit"
+    else
+      new_figure = Figure.create(params["figure"])
+      new_figure.titles << Title.find_or_create_by(params["title"]) if valid(params["title"]["name"])
+      new_figure.landmarks << Landmark.find_or_create_by(params["landmark"]) if valid(params["landmark"]["name"])
+      new_figure.save
+
+      redirect :"/figures/#{new_figure.id}"
+    end
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :"figures/edit"
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params["figure"])
+    figure.titles << Title.find_or_create_by(params["title"]) if valid(params["title"]["name"])
+    figure.landmarks << Landmark.find_or_create_by(params["landmark"]) if valid(params["landmark"]["name"])
+    figure.save
+
+    redirect :"/figures/#{figure.id}"
+  end
+
+  helpers do
+    def valid(input)
+      !input.strip.empty?
+    end
+  end
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,4 +10,31 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
+  post '/landmarks' do
+    landmark = Landmark.find_by(name: params["landmark"]["name"])
+
+    if landmark
+      redirect :"/landmarks/:id/edit"
+    else
+      new_landmark = Landmark.create(params["landmark"])
+      redirect :"/landmarks/:id"
+    end
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params['landmark'])
+
+    redirect :"/landmarks/#{landmark.id}"
+  end
 end
