d4cb905809fad3288823959a4122075d2af1421b
github username <github email address>
2019-12-16 12:01:30 +0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,14 +1,15 @@
 class FiguresController < ApplicationController
-  get '/figures' do 
-    @figures = Figures.all
+
+  get '/figures' do
+    @figures = Figure.all
     erb :'/figures/index'
   end
 
-  get '/figures/new' do 
-    erb :'figures/new'
+  get '/figures/new' do
+    erb :'/figures/new'
   end
 
-  post '/figures' do 
+  post '/figures' do
     @figure = Figure.new(params[:figure])
 
     if !params[:title][:name].empty?
@@ -20,6 +21,33 @@ class FiguresController < ApplicationController
     end
 
     @figure.save
-    redirect to "figures/#{@figure.id}"
+    
+    redirect :"/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles.build(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks.build(name: params[:landmark][:name])
+    end
+
+    @figure.save
+    redirect :"/figures/#{@figure.id}"
   end
 end
