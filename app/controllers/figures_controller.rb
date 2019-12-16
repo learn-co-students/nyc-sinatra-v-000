f3cfcc71af25c76a1a53603f7e2b6080fab705c8
github username <github email address>
2019-12-16 10:25:22 +0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,24 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do 
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do 
+    erb :'figures/new'
+  end
+
+  post '/figures' do 
+    @figure = Figure.new(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles.build(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks.build(name: params[:landmark][:name])
+    end
+
+    @figure.save
+    redirect to "figures/#{@figure.id}"
+  end
 end
