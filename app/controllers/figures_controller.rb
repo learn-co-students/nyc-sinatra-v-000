aba0eaa027bd6086c2001d449aa051ef94c07ccf
dejarvzal <diannejarvisj@gmail.com>
2020-05-26 23:17:16 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,20 @@
 class FiguresController < ApplicationController
   # add controller methods
 
-  get '/figures' do
-  
+  get '/figures/new' do
+   
+    erb :"/figures/new"
   end
+
+  post '/figures/new' do
+    @figure = Figure.create(params[:figure_name])
+  end
+
 end
+
+
+
+
+
+
+
