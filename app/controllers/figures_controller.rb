85b7e5037de5bffa7f0e5c49e44e894afb100435
Laura Messa Travin <laura.travin@gmail.com>
2021-03-03 04:46:55 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,9 +10,55 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
-  post '/figures'  do
-     binding.pry
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
 
+  post '/figures'  do
+    #  binding.pry
+    #  params with existing data
+    #  {"figure"=>{"name"=>"Juan", "title_ids"=>["2"], "landmark_ids"=>["6"]},
+    #   "title"=>{"name"=>""},
+    #    "Landmark"=>{"name"=>"", "year_completed"=>""}}
+
+           @figure = Figure.create(params[:figure])
+           #it let update with  new title or landmark even if you chose some before
+           unless params[:landmark][:name].empty?
+             @figure.landmarks << Landmark.create(params[:landmark])
+           end
+
+          unless params[:title][:name].empty?
+              @figure.titles << Title.create(params[:title])
+          end
+          @figure.save
+          redirect "/figures/#{@figure.id}"
+
+  end
+
+ patch '/figures/:id' do
+         @figure = Figure.find_by_id(params[:id])
+
+        #  params => {"_method"=>"patch",
+        #            "figure"=>  {"name"=>"Meres",
+        #                        "title_ids"=>["1", "2", "3", "4"],
+        #                         "landmark_ids"=>["10"]},
+        #              "id"=>"6"}
+
+       @figure.update(params[:figure])
+
+       unless params[:title][:name].empty?
+        @figure.title_ids << Title.create(params[:title])
+       end
+       unless params[:landmark][:name].empty?
+         @figure.landmark_ids << Landmark.create([:landmark])
+       end
+      @figure.save
+      redirect "figures/#{@figure.id}"
+   end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,4 +5,34 @@ class LandmarksController < ApplicationController
      @landmarks = Landmark.all
      erb :'landmarks/index'
   end
+
+  get '/landmarks/new'  do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    # binding.pry
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+  #create
+  post '/landmark' do
+    @landmark = Landmark.create(params[:landmark])
+
+    redirect "landmarks/#{@landmark.id}"
+  end
+  #update, alternative isntead of patch when only few files
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 end
